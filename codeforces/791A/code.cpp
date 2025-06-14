7a11cbf69bd3c3c760b37425a608ee5cdc58ce3d
Pranshu Gupta <pranshumg@gmail.com>
2025-06-14 21:13:06 +0530

    commit

---
+++ codeforces/791A/code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int yr = 0;
+    while (a <= b) {
+        a *= 3;
+        b *= 2;
+        yr++;
+    }
+
+    cout << yr << endl;
+
+    return 0;
+}
