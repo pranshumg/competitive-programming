6bf36b14b25c86c38754b1b7da3190b699e2a538
Pranshu Gupta <pranshumg@gmail.com>
2025-06-14 20:28:22 +0530

    commit

---
+++ codeforces/514A/code.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    for (int i = 0; i < s.length(); i++) {
+        int d = s[i] - '0';
+        int inv = 9 - d;
+ 
+        if (i == 0 && inv == 0) {
+            continue;
+        }
+        if (inv < d) {
+            s[i] = inv + '0';
+        }
+    }
+ 
+    cout << s;
+    
+    return 0;
+}
