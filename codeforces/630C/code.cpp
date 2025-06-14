2489e5b0cd5f3666f9dc784a46cdb1b4cb8913d2
Pranshu Gupta <pranshumg@gmail.com>
2025-06-14 20:35:25 +0530

    commit

---
+++ codeforces/630C/code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 1;
+    long long ans = 0;
+    while (i <= n) {
+        ans += pow(2, i++);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
