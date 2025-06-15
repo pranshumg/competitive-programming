1743fddc82fa64e64c0d279ec820088e168ea772
Pranshu Gupta <pranshumg@gmail.com>
2025-06-15 13:19:16 +0530

    commit

---
+++ patterns/P13/code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print13(int n) {
+    int num = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << num++ << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print13(n);
+    return 0;
+}
