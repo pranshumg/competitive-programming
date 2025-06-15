cbc59b5014e26db8725c99c2f64186eec9ecfaff
Pranshu Gupta <pranshumg@gmail.com>
2025-06-15 16:17:24 +0530

    commit

---
+++ patterns/P20/code.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print19(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < n - 1 - i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print19(n);
+    return 0;
+}
