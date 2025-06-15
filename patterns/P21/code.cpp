9f15f442ba4be9b744eb840532769f8e6e16c6d1
Pranshu Gupta <pranshumg@gmail.com>
2025-06-15 16:34:57 +0530

    commit

---
+++ patterns/P21/code.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print21(int n) {
+    if (n == 1) {
+        for (int i = 0; i < n; i++) {
+            cout << "*";
+        }
+    } else {
+        for (int i = 0; i < n; i++) {
+            cout << "*";
+        }
+        cout << endl;
+        for (int i = 0; i < n - 2; i++) {
+            cout << "*";
+            for (int j = 0; j < n - 2; j++) {
+                cout << " ";
+            }
+            cout << "*" << endl;
+        }
+        for (int i = 0; i < n; i++) {
+            cout << "*";
+        }
+    }  
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print21(n);
+    return 0;
+}
