699e86d6a3fc840116d4254c8f6ec09c8ac725bf
Pranshu Gupta <pranshumg@gmail.com>
2025-06-16 15:03:42 +0530

    commit

---
+++ maths/armstrong_numbers/code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void armstrong_number(int n) {
+    int og_n = n, count = 0;
+    while (n > 0) {
+        count++;
+        n /= 10;
+    }
+    n = og_n;
+    int sum = 0;
+    while (n > 0) {
+        int ld = n % 10;
+        sum += pow(ld, count);
+        n /= 10;
+    }
+    if (og_n == sum) cout << "True" << endl;
+    else cout << "False" << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    armstrong_number(n);
+    return 0;
+}
