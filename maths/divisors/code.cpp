0d9f85b44075523284c82cbafc730ccb2cec7994
Pranshu Gupta <pranshumg@gmail.com>
2025-06-16 15:25:41 +0530

    commit

---
+++ maths/divisors/code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_divisors(int n) {
+    vector<int> vec;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            vec.emplace_back(i);
+            if (n / i != i) vec.emplace_back(n / i);
+        }
+    }
+    sort(vec.begin(), vec.end());
+    for (auto it : vec) cout << it << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print_divisors(n);
+    return 0;
+}
