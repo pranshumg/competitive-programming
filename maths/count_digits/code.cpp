d5bb2af84e163919855e7d855bb2325c59fd92f6
Pranshu Gupta <pranshumg@gmail.com>
2025-06-16 14:37:34 +0530

    commit

--- maths/count_digits/code.cpp
+++ maths/count_digits/code.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int count_digits(int n) {
+// Method 1
+int count_digits_1(int n) {
     int count = 0;
     while (n > 0) {
         count++;
         n /= 10;
     }
+    return count; 
+}
+
+// Method 2
+int count_digits_2(int n) {
+    int count = (int)log10(n) + 1;
     return count;
 }
 
 int main() {
     int n;
     cin >> n;
-    int ans = count_digits(n);
-    cout << ans << endl;
+    int ans1 = count_digits_1(n), ans2 = count_digits_2(n);
+    cout << "By Method 1:" << ans1 << endl;
+    cout << "By Method 2:" << ans2 << endl;
     return 0;
 }
