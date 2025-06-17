27c4a7ae3734841de4fd01bbc10b7ee12cbcbd73
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 23:12:24 +0530

    commit

---
+++ hashing/highest_lowest_freq_element/code.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(int arr[], int n) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) mpp[arr[i]]++;
+
+    int max_freq = 0, min_freq = n;
+    int max_ele = INT_MIN, min_ele = INT_MAX;
+    for (auto it : mpp) {
+        int element = it.first;
+        int count = it.second;
+
+        if (count < min_freq) {
+            min_freq = count;
+            min_ele = element;
+        }
+        if (count > max_freq) {
+            max_freq = count;
+            max_ele = element;
+        }
+    }
+    cout << max_ele << " " << min_ele << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    func(arr, n);
+    return 0;
+}
+
