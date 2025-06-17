c163649a1757603df58ce2cb1ed00f2a6b5aaa39
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 18:03:14 +0530

    commit

--- recursion/Subsequences_of_sum_k/code.cpp
+++ recursion/Subsequences_of_sum_k/code.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void func(int idx, vector<int> &vec, int s, int sum, int arr[], int n) {
+void func(int idx, vector<int> &vec, int sum, int k, int arr[], int n) {
     if (idx == n) {
-        if (s == sum) {
+        if (sum == k) {
             for (auto it : vec) cout << it << " ";
             cout << endl;
         }
         return;
     }
     vec.emplace_back(arr[idx]);
-    s += arr[idx];
-    func(idx + 1, vec, s, sum, arr, n);
-    s -= arr[idx];
+    sum += arr[idx];
+    func(idx + 1, vec, sum, k, arr, n);
+    sum -= arr[idx];
     vec.pop_back();
-    func(idx + 1, vec, s, sum, arr, n);
+    func(idx + 1, vec, sum, k, arr, n);
 
 }
  
@@ -23,10 +23,10 @@ int main() {
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
-    int sum;
-    cin >> sum;
+    int k;
+    cin >> k;
     vector<int> vec;
-    func(0, vec, 0, sum, arr, n);
+    func(0, vec, 0, k, arr, n);
 
     return 0;
 }
--- recursion/any_Subsequence_of_sum_k/code.cpp
+++ recursion/any_Subsequence_of_sum_k/code.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool func(int idx, vector<int> &vec, int s, int sum, int arr[], int n) {
+bool func(int idx, vector<int> &vec, int sum, int k, int arr[], int n) {
     if (idx == n) {
-        if (s == sum) {
+        if (sum == k) {
             for (auto it : vec) cout << it << " ";
             cout << endl;
             return true;
@@ -11,11 +11,11 @@ bool func(int idx, vector<int> &vec, int s, int sum, int arr[], int n) {
         return false;
     }
     vec.emplace_back(arr[idx]);
-    s += arr[idx];
-    if (func(idx + 1, vec, s, sum, arr, n) == true) return true;
+    sum += arr[idx];
+    if (func(idx + 1, vec, sum, k, arr, n) == true) return true;
     vec.pop_back();
-    s -= arr[idx];
-    if (func(idx + 1, vec, s, sum, arr, n) == true) return true;
+    sum -= arr[idx];
+    if (func(idx + 1, vec, sum, k, arr, n) == true) return true;
     return false;
 }
  
@@ -24,9 +24,9 @@ int main() {
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++) cin >> arr[i];
-    int sum;
-    cin >> sum;
+    int k;
+    cin >> k;
     vector<int> vec;
-    func(0, vec, 0, sum, arr, n);
+    func(0, vec, 0, k, arr, n);
     return 0;
 }
