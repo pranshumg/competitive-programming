8b60b38ca61ac66089988d916d590cbebfe20327
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 11:52:18 +0530

    commit

---
+++ recursion/print_name_n_times/code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print_name_n_times(int n) {
+    if (n == 0) return;
+    cout << "Pranshu" << endl;
+    print_name_n_times(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n; 
+    print_name_n_times(n);
+    return 0;
+}
