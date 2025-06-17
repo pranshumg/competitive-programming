5f27e6c4d08f2fe693918c06d35377901735c274
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 14:41:28 +0530

    commit

--- recursion/factorial/code.cpp
+++ recursion/factorial/code.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int factorial(int n) {
+int func(int n) {
     if (n == 0 || n == 1) return 1;
-    return n * factorial(n - 1);
+    return n * func(n - 1);
 }
 
 int main() {
     int n;
     cin >> n; 
-    cout << factorial(n) << endl;
+    cout << func(n) << endl;
     return 0;
 }
