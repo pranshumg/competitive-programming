e9234c5fa8bfafeb13dd8f32e60973ba9b5a161a
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 14:48:03 +0530

    commit

--- maths/Euclidean Algorithm/code.cpp
+++ maths/Euclidean Algorithm/code.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int GCD(int n1, int n2) {
+int func(int n1, int n2) {
     while (n1 > 0 && n2 > 0) {
         if (n1 > n2) n1 %= n2;
         else n2 %= n1;
@@ -13,7 +13,7 @@ int GCD(int n1, int n2) {
 int main() {
     int n1, n2;
     cin >> n1 >> n2;
-    int ans = GCD(n1, n2);
+    int ans = func(n1, n2);
     cout << ans << endl;
     return 0;
 }
