2dcc0ee8ea4e538088cf9e5eb6d4c08de1e68e07
Pranshu Gupta <pranshumg@gmail.com>
2025-06-17 12:19:40 +0530

    commit

--- recursion/sum_of_first_n_numbers/code.cpp
+++ recursion/sum_of_first_n_numbers/code.cpp
@@ -1,17 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void sum_of_first_n_numbers(int n, int sum) {
-    if (n == 0) {
-        cout << sum << endl;
-        return;
-    }
-    sum_of_first_n_numbers(n - 1, sum + n);
+int sum_of_first_n_numbers(int n) {
+    if (n == 0) return 0;
+    return n + sum_of_first_n_numbers(n - 1);
 }
 
 int main() {
     int n;
     cin >> n; 
-    sum_of_first_n_numbers(n, 0);
+    cout << sum_of_first_n_numbers(n) << endl;
     return 0;
 }
