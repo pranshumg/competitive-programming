8ae107cfa18a7a525d951aa81df60095d90760ec
Pranshu Gupta <pranshumg@gmail.com>
2025-06-18 09:55:09 +0530

    commit

--- sorting/bubble_sort/code.cpp
+++ sorting/bubble_sort/code.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) if (arr[j + 1] < arr[j]) swap(arr[j], arr[j + 1]);
+        int did_swap = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                swap(arr[j], arr[j + 1]);
+                did_swap = 1;
+            }
+        }
+        if (did_swap == 0) break;           
     }
 }
 
