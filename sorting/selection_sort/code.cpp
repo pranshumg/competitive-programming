3e81ed70fa3ece80cbd2b1a73e144b86f59b2bed
Pranshu Gupta <pranshumg@gmail.com>
2025-06-18 09:36:38 +0530

    commit

---
+++ sorting/selection_sort/code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int smallest_idx = i;
+        for (int j = i + 1; j < n; j++) if (arr[j] < arr[smallest_idx]) smallest_idx = j; 
+        swap(arr[i], arr[smallest_idx]);       
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    return 0;
+}
+
