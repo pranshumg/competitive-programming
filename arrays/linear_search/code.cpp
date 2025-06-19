bfd6f01b98a772a02cc6e7c76474126a68a96f7d
Pranshu Gupta <pranshumg@gmail.com>
2025-06-19 18:15:38 +0530

    commit

---
+++ arrays/linear_search/code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linear_search(vector<int> &vec, int n, int k) {
+    for (int i = 0; i < n; i++) if (vec[i] == k) return i;
+    return -1;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) cin >> vec[i];
+    int k;
+    cin >> k;
+    cout << linear_search(vec, n, k);
+    return 0;
+}
