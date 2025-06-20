d2bee57897eb67c4405271121f19664ba0f1d887
Pranshu Gupta <pranshumg@gmail.com>
2025-06-20 16:53:27 +0530

    commit

---
+++ codeforces/116A/code.cpp
@@ -0,0 +1,22 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+
+    vector<int> vec(2 * n);
+    for (int i = 0; i < 2 * n; i++) cin >> vec[i];
+
+    int ans = vec[1], pass = vec[1];
+    for (int i = 3; i < 2 * n; i += 2) {
+        pass = pass - vec[i - 1] + vec[i];
+        if (pass > ans) ans = pass;
+    }
+    cout << ans;
+
+    return 0;
+}
