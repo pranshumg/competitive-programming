07f0ba553a8fe3266ef0d10ee1c4ba6b1eb7432d
Pranshu Gupta <pranshumg@gmail.com>
2025-06-21 17:44:41 +0530

    commit

---
+++ codeforces/231A/code.cpp
@@ -0,0 +1,24 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() { 
+    int n;
+    cin >> n;
+ 
+    vector<int> vec(3 * n);
+    for (int i = 0; i < 3 * n; i++) cin >> vec[i];
+ 
+    int i = 0, ans = 0;
+    while (i < (3 * n)) {
+        int sum = 0, j = i;
+        while (j <= i + 2) sum += vec[j++];
+        if (sum > 1) ans++;
+        i += 3;
+    }
+    cout << ans;
+ 
+    return 0;
+}
