22baf1b932d65adbbc8ea8ad1b5166c71b40b9b9
Pranshu Gupta <pranshumg@gmail.com>
2025-06-21 13:49:13 +0530

    commit

---
+++ codeforces/500A/code.cpp
@@ -0,0 +1,25 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n, t;
+    cin >> n >> t;
+    vector<int> vec(n - 1);
+    for (int i = 0; i < n - 1; i++) cin >> vec[i];
+    int flag = 0;
+    int sum = 1;
+    while (sum < n) {
+        sum += vec[sum - 1];
+        if (t == sum) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1) cout << "YES";
+    else cout << "NO";
+
+    return 0;
+}
