53b26596ce9b2a6aba6b6b0892305594c3994254
Pranshu Gupta <pranshumg@gmail.com>
2025-06-21 15:08:10 +0530

    commit

---
+++ codeforces/1139B/code.cpp
@@ -0,0 +1,21 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+    vector<long long> vec(n);
+    for (int i = 0; i < n; i++) cin >> vec[i];
+    long long sum = vec[n - 1];  
+    int i = n - 2;
+    while (i >= 0) {
+        if (vec[i] >= vec[i + 1]) vec[i] = vec[i + 1] - 1;  
+        if (vec[i] >= 0) sum += vec[i];
+        i--;
+    }
+    cout << sum;
+    return 0;
+}
