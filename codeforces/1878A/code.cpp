a0546e2c4a5aabd92713e81067e45e8b07c3ea34
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 15:06:45 +0530

    commit

---
+++ codeforces/1878A/code.cpp
@@ -0,0 +1,28 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> vec(n);
+        for (int i = 0; i < n; i++) cin >> vec[i];
+        bool flag = false;
+        for (auto it : vec) {
+            if (it == k) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) cout << "YES" << '\n';
+        else cout << "NO" << '\n';
+    }
+
+    return 0;
+}
