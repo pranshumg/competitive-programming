0c11e9c6b349fb4c4e1dc9b828a061d60b009e4a
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 17:54:54 +0530

    commit

---
+++ codeforces/734A/code.cpp
@@ -0,0 +1,21 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int A = 0, D = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'A') A++;
+        else D++; 
+    }
+    if (A > D) cout << "Anton";
+    else if (D > A) cout << "Danik";
+    else cout << "Friendship";
+    return 0;
+}
