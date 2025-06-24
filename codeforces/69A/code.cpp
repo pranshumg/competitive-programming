2d857f217a8c85ea4075187597e6cce8c556c77d
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 18:21:24 +0530

    commit

---
+++ codeforces/69A/code.cpp
@@ -0,0 +1,21 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+    vector<int> vec(3 * n);
+    for (int i = 0; i < 3 * n; i++) cin >> vec[i];
+    int sum_x = 0, sum_y = 0, sum_z = 0;
+    for (int i = 0; i < (3 * n) - 2; i += 3) {
+        sum_x += vec[i];
+        sum_y += vec[i + 1];
+        sum_z += vec[i + 2];
+    }
+    if (sum_x == 0 && sum_y == 0 && sum_z == 0) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
