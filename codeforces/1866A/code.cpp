41d375e3e016437e890a0ff15a54f1974979295f
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 15:10:10 +0530

    commit

---
+++ codeforces/1866A/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) cin >> vec[i];
+    int num = vec[0] > 0 ? vec[0] : -(vec[0]);
+    for (int i = 1; i < n; i++) {
+        if (vec[i] < num && vec[i] >= 0) num = vec[i];
+        else if (vec[i] < 0 && ((0 - vec[i]) < num)) num = -(vec[i]);
+    }
+    cout << num;
+
+    return 0;
+}
