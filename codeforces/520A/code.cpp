3e6ba19945b0332a57702e463c5b33d73e434369
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 20:22:35 +0530

    commit

---
+++ codeforces/520A/code.cpp
@@ -0,0 +1,23 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    bool flag = true;
+    for (int i = 0; i < n; i++) s[i] = tolower(s[i]);
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        if (s.find(ch) == string::npos) {
+            flag = false;
+            break;
+        }
+    }
+    if (flag) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
