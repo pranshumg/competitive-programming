d0404e7f23a05ce24824f276397419b280352387
Pranshu Gupta <pranshumg@gmail.com>
2025-06-24 17:47:48 +0530

    commit

---
+++ codeforces/59A/code.cpp
@@ -0,0 +1,24 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+    string s;
+    cin >> s;
+
+    int upp = 0, low = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= 65 && s[i] <= 90) upp++;
+        else low++;
+    }
+
+    if (upp > low) {
+        for (int i = 0; i < s.size(); i++) cout << (char)toupper(s[i]);
+    } else {
+        for (int i = 0; i < s.size(); i++) cout << (char)tolower(s[i]);
+    }
+
+    return 0;
+}
