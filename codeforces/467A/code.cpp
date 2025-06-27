8cdaf7376823746c42a7dd66e1368d9953f893fe
Pranshu Gupta <pranshumg@gmail.com>
2025-06-27 23:21:10 +0530

    commit

---
+++ codeforces/467A/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> vec(2 * n);
+    for(int i = 0; i < 2 * n; i++) cin >> vec[i];
+    int ans = 0;
+    for (int i = 0; i < 2 * n - 1; i += 2) {
+        if (vec[i + 1] - vec[i] >= 2) ans++;
+    }
+    cout << ans;
+    return 0;
+}
