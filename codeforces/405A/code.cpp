0ace4eac64c4fd9b777dec1b8d8d570d1c25643b
Pranshu Gupta <pranshumg@gmail.com>
2025-06-28 22:56:56 +0530

    commit

---
+++ codeforces/405A/code.cpp
@@ -0,0 +1,17 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    for (auto it : vec) cout << it << " ";
+    return 0;
+}
