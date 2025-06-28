10983ae00c32844c371ba5d99624024bc81c51e9
Pranshu Gupta <pranshumg@gmail.com>
2025-06-28 22:47:58 +0530

    commit

---
+++ codeforces/486A/code.cpp
@@ -0,0 +1,15 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long n;
+    cin >> n;
+    if (n % 2 != 0) cout << (n / 2) * (n / 2 + 1) + -((n / 2 + 1) * (n / 2 + 1)); 
+    else cout << (n / 2) * (n / 2 + 1) + -((n / 2) * (n / 2)); 
+    return 0;
+}
