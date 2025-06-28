605533f1f39e2f5e0b316fef29198e051d30388e
Pranshu Gupta <pranshumg@gmail.com>
2025-06-28 22:46:55 +0530

    commit

---
+++ codeforces/228A/code.cpp
@@ -0,0 +1,16 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> vec(4);
+    for (int i = 0; i < 4; i++) cin >> vec[i];
+    set<int> st;
+    for (int i = 0; i < 4; i++) st.insert(vec[i]);
+    cout << 4 - st.size();
+    return 0;
+}
