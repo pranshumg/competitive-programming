07994ada7cfc034795fb87210b1bdc34fb4c9e73
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 11:57:58 +0530

    commit

---
+++ codeforces/1791A/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        string s = "codeforces";
+        char ch;
+        cin >> ch;
+        if (s.find(ch) == string::npos) cout << "NO" << '\n';
+        else cout << "YES" << '\n';
+    }
+    return 0;
+}
