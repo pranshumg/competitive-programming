2d3ee0b0f5db56d76c7ef2db0a41b31d5711a77f
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 12:18:47 +0530

    commit

---
+++ codeforces/1703A/code.cpp
@@ -0,0 +1,19 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        if ((s[0] == 'Y' || s[0] == 'y') && (s[1] == 'E' || s[1] == 'e') && (s[2] == 'S' || s[2] == 's')) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
