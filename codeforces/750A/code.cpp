eac53da871118eaf1c0e9a20067c1c1985d3473f
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 12:20:43 +0530

    commit

---
+++ codeforces/750A/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    int tl = 240 - k;
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        tl -= 5 * i;
+        if (tl >= 0) ans++;
+    }
+    cout << ans;
+    return 0;
+}
