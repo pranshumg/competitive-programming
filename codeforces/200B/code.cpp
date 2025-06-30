da3d6d6223b7676bcde1bd984190a4003e6e6743
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 12:23:06 +0530

    commit

---
+++ codeforces/200B/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    float sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+        sum += vec[i];
+    }
+    cout << sum / n;
+    return 0;
+}
