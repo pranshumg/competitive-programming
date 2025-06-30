61fcc0a68ec1cef93458815465b5b14f907e510c
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 12:30:26 +0530

    commit

---
+++ codeforces/1512A/code.cpp
@@ -0,0 +1,29 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> vec(n);
+        map<int, int> mpp;
+        for (int i = 0; i < n; i++) {
+            cin >> vec[i];
+            mpp[vec[i]]++;
+        }
+        for (auto it : mpp) {
+            if (it.second == 1) {
+                auto idx = find(vec.begin(), vec.end(), it.first);
+                cout << (idx - vec.begin() + 1) << '\n';
+            }
+        }    
+    }
+    return 0;
+}
