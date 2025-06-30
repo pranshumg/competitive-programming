a283f3b7833af52fbe04d939ba1c78fd29bf82fc
Pranshu Gupta <pranshumg@gmail.com>
2025-06-30 12:26:07 +0530

    commit

---
+++ codeforces/1619A/code.cpp
@@ -0,0 +1,23 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        if (s.size() % 2 != 0) cout << "NO\n";
+        else {
+            string s1 = s.substr(0, s.size() / 2), s2 = s.substr(s.size() / 2, s.size());
+            if (s1 == s2) cout << "YES\n";
+            else cout << "NO\n";
+        }
+    } 
+    return 0;
+}
