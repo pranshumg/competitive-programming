4ee26096a6898476e24d91300110be4b34606071
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 23:22:11 +0530

    commit

--- codeforces/1896A/code.cpp
+++ codeforces/1896A/code.cpp
@@ -13,21 +13,21 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<int> vec(n);
-        for (int i = 0; i < n; i++) cin >> vec[i];  
-        vector<int> sorted_vec = vec;
-        sort(sorted_vec.begin(), sorted_vec.end());
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];  
+        vector<int> sorted_v = v;
+        sort(sorted_v.begin(), sorted_v.end());
         bool swapped = true;
         while (swapped) {
             swapped = false;
             for (int i = 1; i < n - 1; i++) {
-                if (vec[i - 1] < vec[i] && vec[i] > vec[i + 1]) {
-                    swap(vec[i], vec[i + 1]);
+                if (v[i - 1] < v[i] && v[i] > v[i + 1]) {
+                    swap(v[i], v[i + 1]);
                     swapped = true;
                 }
             }
         }
-        if (vec == sorted_vec) cout << "YES" << '\n';
+        if (v == sorted_v) cout << "YES" << '\n';
         else cout << "NO" << '\n';
     }
     return 0;
