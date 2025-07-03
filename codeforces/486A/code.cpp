640ea424482141db9e2669b239a8d0de5f4d07e1
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 22:50:35 +0530

    commit

--- codeforces/486A/code.cpp
+++ codeforces/486A/code.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    long long n;
+    int64_t n;
     cin >> n;
     if (n % 2 != 0) cout << (n / 2) * (n / 2 + 1) + -((n / 2 + 1) * (n / 2 + 1)); 
     else cout << (n / 2) * (n / 2 + 1) + -((n / 2) * (n / 2)); 
