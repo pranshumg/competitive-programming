0a0c5992eb23c88fe06ca5589ddf0044cfe5435c
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 22:42:57 +0530

    commit

--- codeforces/155A/code.cpp
+++ codeforces/155A/code.cpp
@@ -10,17 +10,17 @@ int main() {
     cin.tie(nullptr);
     int n;
     cin >> n;
-    vector<int> vec1(n);
-    for (int i = 0; i < n; i++) cin >> vec1[i];
-    vector<int> vec2;
+    vector<int> v1(n);
+    for (int i = 0; i < n; i++) cin >> v1[i];
+    vector<int> v2;
     int ans = 0;
-    vec2.emplace_back(vec1[0]);
+    v2.emplace_back(v1[0]);
     for (int i = 1; i < n; i++) {
-        int max = *(max_element(vec2.begin(), vec2.end())); 
-        int min = *(min_element(vec2.begin(), vec2.end()));
-        if (vec1[i] > max || vec1[i] < min) {
+        int max = *(max_element(v2.begin(), v2.end())); 
+        int min = *(min_element(v2.begin(), v2.end()));
+        if (v1[i] > max || v1[i] < min) {
             ans++;
-            vec2.emplace_back(vec1[i]);
+            v2.emplace_back(v1[i]);
         }
     }
     cout << ans;
