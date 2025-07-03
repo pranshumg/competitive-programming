1bc5334c0e3cce0c061e9828e262a13aa60ab897
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 23:28:38 +0530

    commit

--- codeforces/263A/code.cpp
+++ codeforces/263A/code.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int arr[5][5], row = 5, col = 5;
+    int a[5][5], row = 5, col = 5;
     for (int i = 0; i < row; i++) {
-        cin >> arr[i][0] >> arr[i][1] >> arr[i][2] >> arr[i][3] >> arr[i][4];
+        cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3] >> a[i][4];
     }
     int x, y;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            if (arr[i][j] == 1) {
+            if (a[i][j] == 1) {
                 x = i;
                 y = j;
                 break;
