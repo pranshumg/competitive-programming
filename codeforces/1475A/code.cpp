28260bad930595cb366081b5f3dc0ada85994dae
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 16:50:02 +0530

    commit

---
+++ codeforces/1475A/code.cpp
@@ -0,0 +1,23 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        if ((n & (n - 1)) == 0) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
+    }
+    return 0;
+}
