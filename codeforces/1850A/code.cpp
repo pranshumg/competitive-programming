b268525521a3a3f9d1de45ae5203c7c9a6c37ca7
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 23:19:22 +0530

    commit

--- codeforces/1850A/code.cpp
+++ codeforces/1850A/code.cpp
@@ -11,10 +11,10 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        vector<int> vec(3);
-        for (int i = 0; i < 3; i++) cin >> vec[i];
-        sort(vec.begin(), vec.end());
-        if (vec[1] + vec[2] >= 10) cout << "YES\n";
+        vector<int> v(3);
+        for (int i = 0; i < 3; i++) cin >> v[i];
+        sort(v.begin(), v.end());
+        if (v[1] + v[2] >= 10) cout << "YES\n";
         else cout << "NO\n";
     }
     return 0;
