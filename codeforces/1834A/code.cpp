dc7ffba425a2ef948e4aef9bdb31ef3d6fe050bb
Pranshu Gupta <pranshumg@gmail.com>
2025-07-03 23:14:03 +0530

    commit

--- codeforces/1834A/code.cpp
+++ codeforces/1834A/code.cpp
@@ -13,19 +13,19 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        vector<int> vec(n);
+        vector<int> v(n);
         int sum = 0, prod = 1;
         for (int i = 0; i < n; i++) {
-            cin >> vec[i];
-            sum += vec[i];
-            prod *= vec[i];
+            cin >> v[i];
+            sum += v[i];
+            prod *= v[i];
         } 
         int ans = 0;
         if (sum >= 0 && prod == -1) ans += 1;
         else if (sum < 0) {
             for (int i = 0; i < n; i++) {
                 if (sum < 0 || prod == -1) {
-                    if (vec[i] == -1) {
+                    if (v[i] == -1) {
                         sum += 2;
                         prod *= -1;
                         ans += 1;
