fc96f3d2828852ef8373e59234c15b3625994dc5
Pranshu Gupta <pranshumg@gmail.com>
2025-07-04 17:02:57 +0530

    commit

---
+++ codeforces/1971A/code.cpp
@@ -0,0 +1,19 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        cout << min(x, y) << " " << max(x, y) << '\n';
+    }
+    return 0;
+}
