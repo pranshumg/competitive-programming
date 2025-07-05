6d6fd789c87f59513da3ca0442ee0019e5e9a9f5
Pranshu Gupta <pranshumg@gmail.com>
2025-07-05 15:32:05 +0530

    commit

---
+++ codeforces/169A/code.cpp
@@ -0,0 +1,20 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h> 
+
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    cout << v[n - a] - v[n - 1 - a];
+    return 0;
+}
