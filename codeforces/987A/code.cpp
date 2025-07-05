6fb3d600d1efabccea4a388d3b0be7af27367341
Pranshu Gupta <pranshumg@gmail.com>
2025-07-05 12:05:59 +0530

    commit

--- codeforces/987A/code.cpp
+++ codeforces/987A/code.cpp
@@ -8,40 +8,39 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    map<string, string> mp = {
-        {"purple", "Power"},
-        {"green", "Time"},
-        {"blue", "Space"},
-        {"orange", "Soul"},
-        {"red", "Reality"},
-        {"yellow", "Mind"}
-    };
     int n;
     cin >> n;
-    if (n == 0) {
-        cout << 6 <<'\n';
-        for (auto it: mp) {
-            cout << it.second << '\n';
-        }
-        return 0;
-    }
-    string curr;
-    vector<string> v(n);
+    string s;
+    int p = 0, g = 0, b = 0, o = 0, r = 0, y = 0;
     for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    vector<string> res;
-    for (auto it1: mp) {
-        for (auto it2: v) {
-            if (find(v.begin(), v.end(), it1.first) == v.end()) {
-                res.emplace_back(it1.second);
-                break;
-            }
+        cin >> s;
+        switch(s[0]) {
+        case 'p':
+            p = 1;
+            break;
+        case 'g':
+            g = 1;
+            break;
+        case 'b':
+            b = 1;
+            break;
+        case 'o':
+            o = 1;
+            break;
+        case 'r':
+            r = 1;
+            break;
+        case 'y':
+            y = 1;
+            break;
         }
     }
     cout << 6 - n << '\n';
-    for (auto it: res) {
-        cout << it << '\n';
-    }
+    if(!p) cout << "Power\n";
+    if(!g) cout << "Time\n";
+    if(!b) cout << "Space\n";
+    if(!o) cout << "Soul\n";
+    if(!r) cout << "Reality\n";
+    if(!y) cout << "Mind\n";
     return 0;
 }
