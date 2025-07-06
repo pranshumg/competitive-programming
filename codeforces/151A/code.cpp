2d24e8750d7123f49bf224e78885a88374251dae
Pranshu Gupta <pranshumg@gmail.com>
2025-07-06 21:44:33 +0530

    commit

--- codeforces/151A/code.cpp
+++ codeforces/151A/code.cpp
@@ -1,19 +1,15 @@
 /**
  *   author: pranshumg
 **/
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
 
 using namespace std;
- 
-int main() {
+
+int main() {    
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n, k, l ,c, d, p, nl, np;
+    int n, k, l, c, d, p, nl, np;
     cin >> n >> k >> l >> c >> d >> p >> nl >> np;
-    int total_milk = (k * l) / nl;
-    int slices_of_lime =  c * d;
-    int salt_needed = p / np;
-    int ans = min({total_milk, slices_of_lime, salt_needed}) / n;
-    cout << ans << '\n';
+    cout << min(k * l / nl, min(c * d, p / np)) / n;
     return 0;
 }
