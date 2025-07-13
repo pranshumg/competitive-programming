ca893f50306bcaed27d41926e87e50397d9bc0b5
Pranshu Gupta <pranshumg@gmail.com>
2025-07-13 09:58:00 +0530

    commit

---
+++ codeforces/959A/code.cpp
@@ -0,0 +1,19 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "Mahmoud";
+    } else {
+        cout << "Ehab";
+    }
+    return 0;
+}
