9091701e3902e7726ef7843cc09074a9b31aadc2
Pranshu Gupta <pranshumg@gmail.com>
2025-07-13 13:35:20 +0530

    commit

---
+++ codeforces/711A/code.cpp
@@ -0,0 +1,42 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<vector<char>> v(n, vector<char>(5));
+    bool flag = false;
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[i].size(); j++) {
+            cin >> v[i][j];
+        }
+        if (!flag) {
+            if (v[i][0] == 'O' && v[i][1] == 'O') {
+                v[i][0] = '+', v[i][1] = '+';
+                flag = true;
+            } else if (v[i][3] == 'O' && v[i][4] == 'O') {
+                v[i][3] = '+', v[i][4] = '+';
+                flag = true;
+            }
+        }
+    }
+    if (flag) {
+        cout << "YES\n";
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = 0; j < v[i].size(); j++) {
+                cout << v[i][j];
+            }
+            cout << '\n';
+        }
+    } else {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
