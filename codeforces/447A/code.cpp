4179c303edbb7085fc1d9dde7c0c71cda62e98e2
Pranshu Gupta <pranshumg@gmail.com>
2025-07-25 19:48:55 +0530

    commit

---
+++ codeforces/447A/code.cpp
@@ -0,0 +1,25 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int p, n;
+    cin >> p >> n;
+    int curr, ans = -1;
+    vector<int> v;
+    for (int i = 1; i <= n; i++) {
+        cin >> curr;
+        if (find(v.begin(), v.end(), curr % p) == v.end()) {
+            v.emplace_back(curr % p);
+        } else if (ans == -1) {
+            ans = i;
+        }
+    }
+    cout << ans;
+    return 0;
+}
