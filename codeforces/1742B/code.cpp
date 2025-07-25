0e3231152b389a4456125fc29158b5a682fca8ee
Pranshu Gupta <pranshumg@gmail.com>
2025-07-26 01:26:57 +0530

    commit

---
+++ codeforces/1742B/code.cpp
@@ -0,0 +1,35 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        bool flag = true;
+        for (int i = 1; i < n; i++) {
+            if (v[i] <= v[i - 1]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
