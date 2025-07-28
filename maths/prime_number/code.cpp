0b8116c3336048d2fb387783b045e6fac4b19061
Pranshu Gupta <pranshumg@gmail.com>
2025-07-29 00:07:24 +0530

    commit

--- maths/prime_number/code.cpp
+++ maths/prime_number/code.cpp
@@ -1,21 +1,45 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void func(int n) {
-    int count = 0;
-    for (int i = 1; i * i <= n; i++) {
+// Method 1
+// TC - O(n)
+void func1(int n) {
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
         if (n % i == 0) {
-            count++;
-            if (n / i != i) count++;
+            cnt++;
         }
     }
-    if (count == 2) cout << "True" << endl;
-    else cout << "False" << endl;
+    if (cnt == 2) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+// Method 2
+// TC - O(√n)
+void func2(int n) {
+    int cnt = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            cnt++;
+            if (n / i != i) {
+                cnt++;
+            }
+        }
+    }
+    if (cnt == 2) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
 }
 
 int main() {
     int n;
     cin >> n;
-    func(n);
+    func2(n);
     return 0;
 }
