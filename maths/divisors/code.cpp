8de2e88c0acb05f0a9d019643f2073ad502637a5
Pranshu Gupta <pranshumg@gmail.com>
2025-07-28 23:48:45 +0530

    commit

--- maths/divisors/code.cpp
+++ maths/divisors/code.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void func(int n) {
-    vector<int> vec;
-    for (int i = 1; i * i <= n; i++) {
+// Method 1
+// TC - O(n)
+void func1(int n) {
+    for (int i = 1; i <= n; i++) {
         if (n % i == 0) {
-            vec.emplace_back(i);
-            if (n / i != i) vec.emplace_back(n / i);
+            cout << i << ' ';
         }
     }
-    sort(vec.begin(), vec.end());
-    for (auto it : vec) cout << it << " ";
+    cout << '\n';
+}
+
+// Method 2
+// TC - O(√n log n)
+/* Finding divisors: O(√n)
+   Sorting divisors: O(√n log n)
+   Printing: O(√n) */
+void func2(int n) {
+    vector<int> v;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            v.push_back(i);
+            if (n / i != i) {
+                v.push_back(n / i);
+            }
+        }
+    }
+    sort(v.begin(), v.end());
+    for (auto it: v) {
+        cout << it << ' ';
+    }
+    cout << '\n';
 }
 
 int main() {
     int n;
     cin >> n;
-    func(n);
+    func2(n);
     return 0;
 }
