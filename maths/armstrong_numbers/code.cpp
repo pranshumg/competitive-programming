9b6de2440d13518935cb08bc637d65e5f7300685
Pranshu Gupta <pranshumg@gmail.com>
2025-07-28 23:21:38 +0530

    commit

--- maths/armstrong_numbers/code.cpp
+++ maths/armstrong_numbers/code.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(log n)
 void func(int n) {
-    int og_n = n, count = 0;
+    int cnt = (int)(log10(n) + 1), sum = 0, og_n = n;
     while (n > 0) {
-        count++;
+        int d = n % 10;
+        sum += pow(d, cnt);
         n /= 10;
     }
-    n = og_n;
-    int sum = 0;
-    while (n > 0) {
-        int ld = n % 10;
-        sum += pow(ld, count);
-        n /= 10;
+    if (sum == og_n) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
     }
-    if (og_n == sum) cout << "True" << endl;
-    else cout << "False" << endl;
 }
 
 int main() {
