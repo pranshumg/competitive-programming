f2206c38144e573a14a731de5295d92d2346046c
Pranshu Gupta <pranshumg@gmail.com>
2025-07-29 00:23:13 +0530

    commit

---
+++ maths/GCD/code.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// TC - O(min(n, m))
+void func(int n, int m) {
+    for (int i = min(n, m); i >= 1; i--) {
+        if (n % i == 0 && m % i == 0) {
+            cout << i << '\n';
+            break;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    func(n, m);
+    return 0;
+}
