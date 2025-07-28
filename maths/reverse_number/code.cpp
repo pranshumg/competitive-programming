b79732284f2df89b6140544dd38b23f39d5cc16f
Pranshu Gupta <pranshumg@gmail.com>
2025-07-28 23:11:27 +0530

    commit

--- maths/reverse_number/code.cpp
+++ maths/reverse_number/code.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(log n)
 int func(int n) {
     int rev_n = 0;
     while (n > 0) {
-        int ld = n % 10;
-        rev_n = rev_n * 10 + ld;
+        int d = n % 10;
+        rev_n = rev_n * 10 + d;
         n /= 10;
     }
     return rev_n;
@@ -14,7 +16,6 @@ int func(int n) {
 int main() {
     int n;
     cin >> n;
-    int ans = func(n);
-    cout << ans << endl;
+    cout << func(n) << '\n';
     return 0;
 }
