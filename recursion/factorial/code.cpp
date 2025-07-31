574a4fcd038ff88878d4686855fbdd321a791f6e
Pranshu Gupta <pranshumg@gmail.com>
2025-07-31 23:29:56 +0530

    commit

--- recursion/factorial/code.cpp
+++ recursion/factorial/code.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(n)
+// SC - O(n)
 int func(int n) {
-    if (n == 0 || n == 1) return 1;
+    if (n == 0 || n == 1) {
+        return 1;
+    }
     return n * func(n - 1);
 }
 
 int main() {
     int n;
-    cin >> n; 
-    cout << func(n) << endl;
+    cin >> n;
+    cout << func(n) << '\n';    
     return 0;
 }
