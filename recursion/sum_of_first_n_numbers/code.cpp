dd750205250e0ed3263ae6da68c48a93810f380f
Pranshu Gupta <pranshumg@gmail.com>
2025-07-31 23:15:58 +0530

    commit

--- recursion/sum_of_first_n_numbers/code.cpp
+++ recursion/sum_of_first_n_numbers/code.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(n)
+// SC - O(n)
 int func(int n) {
-    if (n == 0) return 0;
+    if (n == 0) {
+        return 0;
+    }
     return n + func(n - 1);
 }
 
 int main() {
     int n;
-    cin >> n; 
-    cout << func(n) << endl;
+    cin >> n;
+    cout << func(n) << '\n';    
     return 0;
 }
