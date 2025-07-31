9eb829dcb5fd1359ca76b16e5cc623e431b9885f
Pranshu Gupta <pranshumg@gmail.com>
2025-07-31 22:56:23 +0530

    commit

--- recursion/print_1_to_n/code.cpp
+++ recursion/print_1_to_n/code.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(n)
+// SC - O(n)
 void func(int n) {
-    if (n == 0) return;
+    if (n == 0) {
+        return;
+    }
     func(n - 1);
-    cout << n << " "; 
+    cout << n << ' ';
 }
 
 int main() {
     int n;
-    cin >> n; 
-    func(n);
+    cin >> n;
+    func(n);    
     return 0;
-}   
+}
