fea5eeb6a6c2d531852708c324cf3f1fe085e956
Pranshu Gupta <pranshumg@gmail.com>
2025-07-31 23:51:01 +0530

    commit

--- recursion/string_palindrome/code.cpp
+++ recursion/string_palindrome/code.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool func(int i, string &s) {
-    if (i >= s.size() / 2) return true;
-    if (s[i] != s[s.size() - i - 1]) return false;
-    return func(i + 1, s);
+// TC - O(n)
+// SC - O(n)
+bool func(string s, int i, int n) {
+    if (i >= n / 2) {
+        return true;
+    }
+    if (s[i] != s[n - i - 1]) {
+        return false;
+    }
+    return func(s, i + 1, n);
 }
 
 int main() {
     string s;
     cin >> s;
-    cout << boolalpha << func(0, s);
+    cout << boolalpha << func(s, 0, (int)s.size());    
     return 0;
 }
