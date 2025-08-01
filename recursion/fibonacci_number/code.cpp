7f5182c57b2c5080a38e64b74c5b17fa64888751
Pranshu Gupta <pranshumg@gmail.com>
2025-08-01 14:31:14 +0530

    commit

--- recursion/fibonacci_number/code.cpp
+++ recursion/fibonacci_number/code.cpp
@@ -1,15 +1,22 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+// TC - O(2^n)
+// SC - O(n)
 int func(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
     return func(n - 1) + func(n - 2);
 }
- 
+
 int main() {
     int n;
     cin >> n;
-    cout << func(n) << endl;
+    cout <<  func(n) << '\n';    
     return 0;
 }
