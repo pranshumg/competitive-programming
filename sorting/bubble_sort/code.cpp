051286d16f4294c01a6129e423b94e5cfe7e2a3f
Pranshu Gupta <pranshumg@gmail.com>
2025-08-10 17:02:03 +0530

    commit

--- sorting/bubble_sort/code.cpp
+++ sorting/bubble_sort/code.cpp
@@ -1,25 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void bubble_sort(vector<int> &vec, int n) {
-    for (int i = 0; i < n - 1; i++) {
+// TC :
+// Best - O(n)
+// Worst - O(n²)
+void bubble_sort(vector<int>& v, int n) {
+    for (int i = 0; i < n; i++) {
         int did_swap = 0;
-        for (int j = 0; j < n - i - 1; j++) {
-            if (vec[j + 1] < vec[j]) {
-                swap(vec[j], vec[j + 1]);
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (v[j] > v[j + 1]) {
+                swap(v[j], v[j + 1]);
                 did_swap = 1;
             }
         }
-        if (did_swap == 0) break;           
+        if (!did_swap) {
+            break;
+        }
     }
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    bubble_sort(vec, n);
-    for (auto it : vec) cout << it << " ";
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }    
+    bubble_sort(v, n);
+    for (auto it: v) {
+        cout << it << ' ';
+    }
+    cout << '\n';
     return 0;
 }
