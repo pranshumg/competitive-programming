508329324a23e6667d65d0fe0fc6189c1aa5f8e5
Pranshu Gupta <pranshumg@gmail.com>
2025-08-10 17:05:28 +0530

    commit

--- sorting/bubble_sort_recursion/code.cpp
+++ sorting/bubble_sort_recursion/code.cpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void bubble_sort(vector<int> &vec, int n) {
-    if (n == 1) return;
+// Best TC - O(n)
+// Worst TC - O(n²)
+void bubble_sort_rec(vector<int>& v, int i, int n) {
+    if (i  == n - 1) {
+        return;
+    }
     int did_swap = 0;
-    for (int j = 0; j < n - 1; j++) {
-        if (vec[j + 1] < vec[j]) {
-            swap(vec[j], vec[j + 1]);
+    for (int j = 0; j < n - 1 - i; j++) {
+        if (v[j] > v[j + 1]) {
+            swap(v[j], v[j + 1]);
             did_swap = 1;
         }
     }
-    if (did_swap == 0) return;
-    bubble_sort(vec, n - 1);
+    if (!did_swap) {
+        return;
+    }
+    bubble_sort_rec(v, i + 1, n);
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    bubble_sort(vec, n);
-    for (auto it : vec) cout << it << " ";
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }    
+    bubble_sort_rec(v, 0, n);
+    for (auto it: v) {
+        cout << it << ' ';
+    }
+    cout << '\n';
     return 0;
 }
