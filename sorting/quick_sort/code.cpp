ed1b33b4dcefa1f7b5a35f54f96924dac704b88a
Pranshu Gupta <pranshumg@gmail.com>
2025-08-12 19:03:29 +0530

    commit

--- sorting/quick_sort/code.cpp
+++ sorting/quick_sort/code.cpp
@@ -1,31 +1,46 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int partition(vector<int> &vec, int low, int high) {
-    int pivot = vec[low], i = low, j = high;
+// TC - O(n log n)
+// SC - O(log n)
+int partition(vector<int>& v, int low, int high) {
+    int pivot = v[low], i = low, j = high;
     while (i < j) {
-        while (vec[i] <= pivot && i < high) i++;
-        while (vec[j] >= pivot && j > low) j--;
-        if (i < j) swap(vec[i], vec[j]);
+        while (v[i] <= pivot && i <= high) {
+            i++;
+        }
+        while (v[j] > pivot && j >= low) {
+            j--;
+        }
+        if (i < j) {
+            swap(v[i], v[j]);
+        }
     }
-    swap(vec[low], vec[j]);
+    swap(v[low], v[j]);
     return j;
 }
 
-void quick_sort(vector<int> &vec, int low, int high) {
-    if (low < high) {
-        int part_idx = partition(vec, low, high);
-        quick_sort(vec, low, part_idx - 1);
-        quick_sort(vec, part_idx + 1, high);
+void quick_sort(vector<int>& v, int low, int high) {
+    if (low >= high) {
+        return ;
     }
+    int pivot_idx = partition(v, low, high);
+    quick_sort(v, low, pivot_idx - 1);
+    quick_sort(v, pivot_idx + 1, high);
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    quick_sort(vec, 0, n - 1);
-    for (auto it : vec) cout << it << " ";
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    quick_sort(v, 0, n - 1);
+    for (auto it: v) {
+        cout << it << ' ';
+    }
+    cout << '\n';
     return 0;
 }
