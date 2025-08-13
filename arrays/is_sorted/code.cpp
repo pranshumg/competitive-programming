fb14ca2e0c8d3b582e2469e8411a4cfb33a9d41e
Pranshu Gupta <pranshumg@gmail.com>
2025-08-13 17:06:10 +0530

    commit

--- arrays/is_sorted/code.cpp
+++ arrays/is_sorted/code.cpp
@@ -1,20 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-bool is_sorted(vector<int> &vec, int n) {
-    for (int i = 1; i < n; i++) {
-        if (vec[i] >= vec[i - 1]) {}
-        else return false;
+/* To check array is sorted in non-decreasing order */
+// TC - O(n), SC - O(1)
+bool is_sorted(vector<int>& v, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (v[i] > v[i + 1]) {
+            return false;
+        }
     }
     return true;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    bool ans = is_sorted(vec, n);
-    cout << boolalpha << ans;
-    return 0;
 }
