a94dcfe7dea2f66c3e5d2a7b8684fa50a0fd3017
Pranshu Gupta <pranshumg@gmail.com>
2025-08-14 15:51:39 +0530

    commit

--- arrays/union/code.cpp
+++ arrays/union/code.cpp
@@ -1,71 +1,50 @@
-// Optimal:
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
-vector<int> union_of_sorted_arrays(vector<int> &vec1, int n1, vector<int> &vec2, int n2) {
-    int i = 0, j = 0;
-    vector<int> union_vec;
-    while (i < n1 && j < n2) {
-        if (vec1[i] <= vec2[j]) {
-            if (union_vec.size() == 0 || union_vec.back() != vec1[i]) union_vec.emplace_back(vec1[i]);       
+/* Union of two sorted arrays */
+// Brute (TC - O((n + m)log(n + m)), SC - O(n + m))
+vector<int> Union(vector<int>& v1, vector<int>& v2) {
+    set<int> st;
+    int n = (int)v1.size(), m = (int)v2.size();
+    for (int i = 0; i < n; i++) {
+        st.insert(v1[i]);
+    }
+    for (int i = 0; i < m; i++) {
+        st.insert(v2[i]);
+    }
+    vector<int> ans(st.begin(), st.end());
+    return ans;
+}
+
+// Optimal (TC - O(n + m), SC - O(1))
+vector<int> Union(vector<int>& v1, vector<int>& v2) {
+    vector<int> ans;
+    int n = (int)v1.size(), m = (int)v2.size(), i = 0, j = 0;
+    while (i < n && j < m) {
+        if (v1[i] <= v2[j]) {
+            if (ans.size() == 0 || ans.back() != v1[i]) {
+                ans.push_back(v1[i]);
+            }
             i++;
         } else {
-            if (union_vec.size() == 0 || union_vec.back() != vec2[j]) union_vec.emplace_back(vec2[j++]);
+            if (ans.size() == 0 || ans.back() != v2[j]) {
+                ans.push_back(v2[j]);
+            }
             j++;
         }
     }
-    while (i < n1) {
-        if (union_vec.size() == 0 || union_vec.back() != vec1[i]) union_vec.emplace_back(vec1[i]);
+    while (i < n) {
+        if (ans.size() == 0 || ans.back() != v1[i]) {
+            ans.push_back(v1[i]);
+        }
         i++;
-    }
-    while (j < n2) {
-        if (union_vec.size() == 0 || union_vec.back() != vec2[j]) union_vec.emplace_back(vec2[j]);
+    } 
+    while (j < m) {
+        if (ans.size() == 0 || ans.back() != v2[j]) {
+            ans.push_back(v2[j]);
+        }
         j++;
     }
-    return union_vec;
-} 
-
-int main() { 
-    int n1;
-    cin >> n1;
-    vector<int> vec1(n1);
-    for (int i = 0; i < n1; i++) cin >> vec1[i];
-    int n2;
-    cin >> n2;
-    vector<int> vec2(n2);
-    for (int i = 0; i < n2; i++) cin >> vec2[i];
-    vector<int> union_vec = union_of_sorted_arrays(vec1, n1, vec2, n2);
-    for (auto it : union_vec) cout << it << " ";
-    return 0;
+    return ans;
 }
-
-// Brute Force:
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> union_of_sorted_arrays(vector<int> &vec1, int n1, vector<int> &vec2, int n2) {
-    set<int> st;
-    for (int i = 0; i < n1; i++) st.insert(vec1[i]);
-    for (int i = 0; i < n2; i++) st.insert(vec2[i]); 
-    vector<int> union_vec;
-    for (auto it : st) union_vec.emplace_back(it);
-    return union_vec;
-} 
-
-int main() { 
-    int n1;
-    cin >> n1;
-    vector<int> vec1(n1);
-    for (int i = 0; i < n1; i++) cin >> vec1[i];
-    int n2;
-    cin >> n2;
-    vector<int> vec2(n2);
-    for (int i = 0; i < n2; i++) cin >> vec2[i];
-    vector<int> union_vec = union_of_sorted_arrays(vec1, n1, vec2, n2);
-    for (auto it : union_vec) cout << it << " ";
-    return 0;
-}
-*/
