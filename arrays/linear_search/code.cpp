f83b928dee5f42c7891caed9da489d1f88c3ac72
Pranshu Gupta <pranshumg@gmail.com>
2025-08-14 15:17:45 +0530

    commit

--- arrays/linear_search/code.cpp
+++ arrays/linear_search/code.cpp
@@ -1,18 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int linear_search(vector<int> &vec, int n, int k) {
-    for (int i = 0; i < n; i++) if (vec[i] == k) return i;
+/* Linear search */
+// TC - O(n), SC - O(1)
+int linear_search(vector<int>& v, int n, int k) {
+    for (int i = 0; i < n; i++) {
+        if (v[i] == k) {
+            return i;
+        }
+    }
     return -1;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    int k;
-    cin >> k;
-    cout << linear_search(vec, n, k);
-    return 0;
 }
