e60ce5247c2821708d80a243ab30ecc24d61f9a5
Pranshu Gupta <pranshumg@gmail.com>
2025-08-14 19:29:21 +0530

    commit

--- arrays/single_number/code.cpp
+++ arrays/single_number/code.cpp
@@ -1,67 +1,41 @@
-// Brute Force:
-
-/*
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void single_number(vector<int> &vec, int n) {
+/* Number that appears once, and the others twice */
+// Brute (TC - O(n²), SC - O(1))
+int single_number(vector<int>& v, int n) {
     for (int i = 0; i < n; i++) {
         int cnt = 0;
         for (int j = 0; j < n; j++) {
-            if (vec[i] == vec[j]) cnt++;
+            if (v[i] == v[j]) {
+                cnt++;
+            }
         }
-        if (cnt == 1) cout << vec[i];
+        if (cnt == 1) {
+            return v[i];
+        }
     }
 }
 
-int main() { 
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    single_number(vec, n);
-    return 0;
-}
-*/
-
-// Hashing:
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-void single_number(vector<int> &vec, int n) {
-    map<long long, int> mpp;
-    for (int i = 0; i < n; i++) mpp[vec[i]]++;
-    for (auto it : mpp) if (it.second == 1) cout << it.first;
+// Better (TC - O(n), SC - O(n))
+int single_number(vector<int>& v, int n) {
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        mp[v[i]]++;
+    }
+    for (auto it: mp) {
+        if (it.second == 1) {
+            return it.first;
+        }
+    }
 }
 
-int main() { 
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    single_number(vec, n);
-    return 0;
-}
-*/
-
-// Optimal:
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int single_number(vector<int> &vec, int n) {
+// Optimal (TC - O(n), SC - O(1))
+int single_number(vector<int>& v, int n) {
     int XOR = 0;
-    for (int i = 0; i < n; i++) XOR ^= vec[i];
+    for (int i = 0; i < n; i++) {
+        XOR ^= v[i];
+    }
     return XOR;
-}
-
-int main() { 
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    cout << single_number(vec, n);
-    return 0;
 }
