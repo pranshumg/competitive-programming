3b7e1592acf3144e6b107acf0efb7ecef6036cfe
Pranshu Gupta <pranshumg@gmail.com>
2025-08-14 13:48:02 +0530

    commit

--- arrays/move_zeros_to_end/code.cpp
+++ arrays/move_zeros_to_end/code.cpp
@@ -1,51 +1,36 @@
-// Optimal:
-
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void move_zeros_to_end(vector<int> &vec, int n) {
-    int j;
+/* Move zeros to end */
+// Brute (TC - O(n), SC - O(n))
+void move_zeros_to_end(vector<int>& v, int n) {
+    vector<int> temp;
     for (int i = 0; i < n; i++) {
-        if (vec[i] == 0) {
+        if (v[i] != 0) {
+            temp.push_back(v[i]);
+        }
+    }
+    for (int i = 0; i < (int)temp.size(); i++) {
+        v[i] = temp[i];
+    }
+    for (int i = (int)temp.size(); i < n; i++) {
+        v[i] = 0;
+    }
+}
+
+// Optimal (TC - O(n), SC - O(1))
+void move_zeros_to_end(vector<int>& v, int n) {
+    int j = -1;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == 0) {
             j = i;
             break;
         }
     }
     for (int i = j + 1; i < n; i++) {
-        if (vec[i] != 0) swap(vec[i], vec[j++]);
+        if (v[i] != 0) {
+            swap(v[i], v[j++]);
+        }
     }
 }
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    move_zeros_to_end(vec, n);
-    for (auto it : vec) cout << it << " ";
-    return 0;
-}
-
-// Brute Force:
-
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-void move_zeros_to_end(vector<int> &vec, int n) {
-    vector<int> temp;
-    for (int i = 0; i < n; i++) if (vec[i] != 0) temp.emplace_back(vec[i]);
-    for (int i = 0; i < temp.size(); i++) vec[i] = temp[i];
-    for (int i = temp.size(); i < n; i++) vec[i] = 0;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) cin >> vec[i];
-    move_zeros_to_end(vec, n);
-    for (auto it : vec) cout << it << " ";
-    return 0;
-}
-*/
