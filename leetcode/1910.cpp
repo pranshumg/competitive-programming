73605ebbe783a3657810cffdfeed2b58141a10c7
Pranshu Gupta <pranshumg@gmail.com>
2025-08-15 17:43:02 +0530

    commit

--- leetcode/1910.cpp
+++ leetcode/1910.cpp
@@ -1,25 +1,13 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while (s.length() > 0 && (s.find(part) < s.length())) {
-            s.erase(s.find(part), part.length());
+        while (s.size() > 0 && s.find(part) != string::npos) {
+            s.erase(s.find(part), part.size());
         }
-        return s;       
+        return s;
     }
 };
-
-int main() {
-    string s = "leetcode", part = "leet";
-
-    cout << "s: " << s << endl;
-    cout << "part: " << part << endl;
-
-    Solution a;
-    cout << a.removeOccurrences(s, part) << endl;
-    
-    return 0;
-}
