c250241d29d1e5ecbcdbc021575474ae4fb27a94
Pranshu Gupta <pranshumg@gmail.com>
2025-08-15 17:41:17 +0530

    commit

--- leetcode/344.cpp
+++ leetcode/344.cpp
@@ -1,36 +1,13 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int start = 0, end = s.size() - 1;
-        while (start < end) {
-            swap(s[start++], s[end--]);
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            swap(s[left++], s[right--]);
         }
     }
 };
-
-void display(vector<char> s) {
-    for (char ch : s) {
-        cout << ch;
-    }
-    cout << endl;
-}
-
-int main() {
-    vector<char> s = {'l', 'e', 'e', 't', 'c', 'o', 'd' ,'e'};
-
-    cout << "s: ";
-    display(s);
-
-    Solution a;
-    a.reverseString(s);
-
-    cout << "reverse: ";
-    display(s);
-    
-    return 0;
-
-}
