3ff13cf2ec111c0c0e38b2f62d4df36939ac3de4
Pranshu Gupta <pranshumg@gmail.com>
2025-08-15 17:42:04 +0530

    commit

--- leetcode/509.cpp
+++ leetcode/509.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
 
 class Solution {
 public:
     int fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return fib(n - 1) + fib(n - 2);
+        if (n == 0 || n == 1) {
+            return n;
         }
+        return fib(n - 1) + fib(n - 2);      
     }
 };
-
-int main() {
-    int n;
-    cout << "n: ";
-    cin >> n;
-    
-    Solution a;
-    cout << "F(" << n << "): " << a.fib(n) << endl;
-
-    return 0;
-}
