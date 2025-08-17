844b0b4b818d6e6ba34f37a13ce8c47a22b9d6a5
Pranshu Gupta <pranshumg@gmail.com>
2025-08-17 14:57:32 +0530

    commit

---
+++ codeforces/1337A/code.cpp
@@ -0,0 +1,23 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << ' ' << c << ' ' << c << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
+    }
+    return 0;
+}
