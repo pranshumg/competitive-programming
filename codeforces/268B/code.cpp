97c113bc87202f0f598a40a6fd80017f2f248d46
Pranshu Gupta <pranshumg@gmail.com>
2025-08-18 20:08:55 +0530

    commit

--- codeforces/268B/code.cpp
+++ codeforces/268B/code.cpp
@@ -8,8 +8,8 @@ using namespace std;
 void run_case() {
     int n;
     cin >> n;
-    int ans = n;
-    for (int i = 2; i <= n; i++) {
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
         ans += (n - i) * i + 1;
     }
     cout << ans << '\n';
