256b0bbc707248b1bf7ba3b21aee49e7bdbe16d4
Pranshu Gupta <pranshumg@gmail.com>
2025-08-24 19:43:19 +0530

    commit

--- codeforces/Problems/610A/code.cpp
+++ codeforces/Problems/610A/code.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void run_case() {
     int64_t n;
     cin >> n;
-    if ((n - 2) % 2 != 0) {
+    if (n % 2 != 0) {
         cout << 0;
     } else if (n % 4 == 0) {
         cout << n / 4 - 1;
