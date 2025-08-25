0636d5c3a02933fb64f6fccd13c9582739b7e4f1
Pranshu Gupta <pranshumg@gmail.com>
2025-08-25 19:40:44 +0530

    commit

---
+++ codeforces/Problems/1038B/code.cpp
@@ -0,0 +1,41 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int n;
+    cin >> n;
+    bool flag = false;
+    int i;
+    for (i = 2; i <= n; i++) {
+        if ((((n * (n + 1)) / 2) - i) % i == 0) {
+            flag = true;
+            break;
+        }
+    }
+    if (flag) {
+        cout << "Yes\n" << 1 << ' ' << i << '\n' << n - 1 << ' ';
+        for (int j = 1; j < i; j++) {
+            cout << j << ' ';
+        }
+        for (int j = i + 1; j <= n; j++) {
+            cout << j << ' ';
+        }
+        return;
+    } 
+    cout << "No\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int tests;
+    // cin >> tests;
+    // while (tests--) {
+        run_case();
+    // }
+    return 0;
+}
