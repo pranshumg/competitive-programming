beb36a2f7b212ab881e8975de1cf42ffbcdcffce
Pranshu Gupta <pranshumg@gmail.com>
2025-09-04 20:12:55 +0530

    commit

---
+++ codeforces/Problems/2094C/code.cpp
@@ -0,0 +1,38 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int n;
+    cin >> n;
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int curr;
+            cin >> curr;
+            if (j == 0 || i == n - 1) {
+                ans.push_back(curr);
+            }
+        }
+    }
+    int sum = accumulate(ans.begin(), ans.end(), 0);
+    cout << (2 * n * (2 * n + 1)) / 2  - sum << ' ';
+    for (auto it: ans) {
+        cout << it << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
+    }
+    return 0;
+}
