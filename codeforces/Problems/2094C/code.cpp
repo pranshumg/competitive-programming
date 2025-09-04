840cde80c5e0f653d244a9cbf15a286816ff13fd
Pranshu Gupta <pranshumg@gmail.com>
2025-09-05 04:05:14 +0530

    commit

--- codeforces/Problems/2094C/code.cpp
+++ codeforces/Problems/2094C/code.cpp
@@ -9,16 +9,17 @@ void run_case() {
     int n;
     cin >> n;
     vector<int> ans;
+    int sum = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             int curr;
             cin >> curr;
             if (j == 0 || i == n - 1) {
                 ans.push_back(curr);
+                sum += curr;
             }
         }
     }
-    int sum = accumulate(ans.begin(), ans.end(), 0);
     cout << (2 * n * (2 * n + 1)) / 2  - sum << ' ';
     for (auto it: ans) {
         cout << it << ' ';
