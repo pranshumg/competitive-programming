7a8b75fd2df3fde6d4ac83fac8e2419d38a5c368
Pranshu Gupta <pranshumg@gmail.com>
2025-09-05 04:03:23 +0530

    commit

--- codeforces/Problems/2094B/code.cpp
+++ codeforces/Problems/2094B/code.cpp
@@ -1,25 +1,28 @@
 /**
  *   author: pranshumg
 **/
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {    
+void run_case() {
+    int n, m, l, r;
+    cin >> n >> m >> l >> r;
+    r -= n - m;
+    if (r >= 0) {
+        cout << l << ' ' << r << '\n';
+        return;
+    }
+    cout << l - r << ' ' << 0 << '\n';
+}
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, l, r;
-        cin >> n >> m >> l >> r;
-        if (r >= n - m) {
-            r -= n - m;
-        } else {
-            l += n - m - r;
-            r = 0;
-        }
-        cout << l << ' ' << r << '\n';
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
     }
     return 0;
 }
