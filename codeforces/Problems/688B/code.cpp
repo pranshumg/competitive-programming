f702b059890904e90195c6550e98f8669f79966a
Pranshu Gupta <pranshumg@gmail.com>
2025-09-05 13:25:56 +0530

    commit

---
+++ codeforces/Problems/688B/code.cpp
@@ -0,0 +1,25 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    string s;
+    cin >> s;
+    cout << s;
+    reverse(s.begin(), s.end());
+    cout << s;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int tests;
+    // cin >> tests;
+    // while (tests--) {
+        run_case();
+    // }
+    return 0;
+}
