e6ab37dadb6713f6d8604994a2e78e93302be7a1
Pranshu Gupta <pranshumg@gmail.com>
2025-09-20 19:26:43 +0530

    commit

---
+++ codeforces/Problems/270A/code.cpp
@@ -0,0 +1,23 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int n;
+    cin >> n;
+    cout << (360 % (180 - n) == 0 ? "YES\n" : "NO\n");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
+    }
+    return 0;
+}
