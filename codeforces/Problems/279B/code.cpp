f064c5b770773d6ecb69e8b1ec797a39d883d2e6
Pranshu Gupta <pranshumg@gmail.com>
2025-09-22 11:18:33 +0530

    commit

---
+++ codeforces/Problems/279B/code.cpp
@@ -0,0 +1,39 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int n, t;
+    cin >> n >> t;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    int left = 0, right = 0, sum = 0, ans = 0;
+    while (right < n) {
+        sum += v[right];
+        if (left == 0 && sum <= t) {
+            ans = right + 1;
+        } 
+        if (sum > t) {
+            sum -= v[left++];
+        }
+        ans = max(ans, right - left + 1);
+        right++;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int tests;
+    // cin >> tests;
+    // while (tests--) {
+        run_case();
+    // }
+    return 0;
+}
