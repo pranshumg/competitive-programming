e778e921595c43b668dac0af131aba47ba257340
Pranshu Gupta <pranshumg@gmail.com>
2025-09-24 18:23:54 +0530

    commit

---
+++ codeforces/Problems/1843A/code.cpp
@@ -0,0 +1,33 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int low = 0, high = n - 1, ans = 0;
+    while (low < high) {
+        ans += v[high--] - v[low++]; 
+    }
+    cout << ans << '\n';
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
+    }
+    return 0;
+}
