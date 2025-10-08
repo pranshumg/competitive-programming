6d5e4f779a5626b7106fd152a4eb92fefba1b8f5
Pranshu Gupta <pranshumg@gmail.com>
2025-10-08 18:53:20 +0530

    commit

--- arrays/buy_and_sell_stock/code.cpp
+++ arrays/buy_and_sell_stock/code.cpp
@@ -5,10 +5,10 @@ using namespace std;
 /* Best time to buy and sell stock */
 // TC - O(n), SC - O(1)
 int max_profit(vector<int>& v, int n) {
-    int mini = v[0], profit = 0;
-    for (int i = 0; i < n; i++) {
-        profit = max(profit, v[i] - mini);
-        mini = min(mini, v[i]);
-    }
-    return profit;
+  int mini = v[0], profit = 0;
+  for (int i = 0; i < n; i++) {
+      profit = max(profit, v[i] - mini);
+      mini = min(mini, v[i]);
+  }
+  return profit;
 }
