a4d90485a4956e27637fb4ae8b8b80cd1ae8ff1e
Pranshu Gupta <pranshumg@gmail.com>
2025-10-08 12:37:51 +0530

    commit

---
+++ template/c++.cpp
@@ -0,0 +1,24 @@
+/**
+ *   author: pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+void run_case() {
+    ${0}
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tests;
+    cin >> tests;
+    while (tests--) {
+        run_case();
+    }
+    return 0;
+}
