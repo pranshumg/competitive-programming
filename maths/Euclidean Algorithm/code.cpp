1bff28b9997204690c2510c3cc7b75ca4cd9a15a
Pranshu Gupta <pranshumg@gmail.com>
2025-10-08 21:35:45 +0530

    commit

--- maths/Euclidean Algorithm/code.cpp
+++ maths/Euclidean Algorithm/code.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // TC - O(log min(n, m))
 int euclidean_algorithm(int n, int m) {
   while (n > 0 && m > 0) {
-    n > m ? n % m : m % n;
+    n > m ? n %= m : m %= n;
   }
   return (n == 0 ? m : n);
 }
