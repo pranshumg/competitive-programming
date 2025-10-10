ed3376a9765b2d3c79100faa992fbc3b19908a67
Pranshu Gupta <pranshumg@gmail.com>
2025-10-10 09:34:16 +0530

    commit

--- template/cp_template.cpp
+++ template/cp_template.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define ceil_div(a, b) (((a) + (b) - 1) / (b))
+
 void run_case() {
   ${0}
 }
