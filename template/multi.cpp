eb298b7a4fc6fb5ea355e9ab15c4c5cf8957f4c5
Pranshu Gupta <pranshumg@gmail.com>
2025-10-19 18:51:30 +0530

    templates for cp

---
+++ template/multi.cpp
@@ -0,0 +1,21 @@
+/**
+ *    author:  pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+  ${0}
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tests;
+  cin >> tests;
+  while (tests--) {
+    run_case();
+  }
+  return 0;
+}
---
+++ template/single.cpp
@@ -0,0 +1,17 @@
+/**
+ *    author:  pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run_case() {
+  ${0}
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  run_case();
+  return 0;
+}
