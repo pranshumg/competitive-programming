fa26061904bc4addd035267e1a0b4f823660cd4b
Pranshu Gupta <pranshumg@gmail.com>
2025-10-30 17:33:05 +0530

    new template

---
+++ template/template.cpp
@@ -0,0 +1,52 @@
+/**
+ *    author:  pranshumg
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+using VI = vector<int>;
+using VL = vector<ll>;
+using VS = vector<string>;
+using VVI = vector<vector<int>>;
+using VVL = vector<vector<ll>>;
+using VPII = vector<pair<int, int>>;
+using VPLL = vector<pair<ll, ll>>;
+using PII = pair<int, int>;
+using PLL = pair<ll, ll>;
+using MII = map<int, int>;
+using MLL = map<ll, ll>;
+using MCI = map<char, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define REP(i, a, b) for (int i = (a); i < (b); ++i)
+#define REPD(i, a, b) for (int i = (a); i >= (b); --i)
+#define EACH(x, a) for (auto &x : (a))
+#define SZ(x) (int)((x).size())
+#define PB push_back
+#define EB emplace_back
+#define PP pop_back
+#define MP make_pair
+#define F first
+#define S second
+
+void run_case() {
+  ${0}
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tests;
+  cin >> tests;
+  for (int tc = 1; tc <= tests; ++tc) {
+    cout << "Case #" << tc << ": ";
+    run_case();
+  }
+  return 0;
+}
