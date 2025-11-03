d120487204d97c099ec03e3097962d189d1da78c
Pranshu Gupta <pranshumg@gmail.com>
2025-11-03 14:52:50 +0530

    delete kth node

---
+++ linked_list/singly_ll/deletion/kth.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete kth node in a singly linked list */
+
+class Node {
+public:
+  int data;
+  Node* next;
+
+  Node(int val) {
+    data = val;
+    next = nullptr;
+  }
+};
+
+// TC - O(n), SC - O(1)
+Node* delete_kth(Node* head, int k) {
+  if (head == nullptr) {
+    return head;
+  }
+  if (k == 1) {
+    Node* tmp = head;
+    head = head->next;
+    delete tmp;
+    return head;
+  }
+  int cnt = 0;
+  Node* tmp = head;
+  Node* prev = NULL;
+  while (tmp != NULL) {
+    ++cnt;
+    if (cnt == k) {
+      prev->next = tmp->next;
+      delete tmp;
+      break;
+    }
+    prev = tmp;
+    tmp = tmp->next;
+  }
+  return head;
+}
