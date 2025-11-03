a05cf55cb243096264601dfea7b8cc2495443001
Pranshu Gupta <pranshumg@gmail.com>
2025-11-03 14:11:48 +0530

    delete head

---
+++ linked_list/singly_ll/deletion/head.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete head in a singly linked list */
+
+class Node {
+public:
+  int data;
+  Node* next;
+
+  Node(int val) {
+    data = val;
+    next = nullptr;
+  }
+};
+
+// TC - O(1), SC - O(1)
+Node* delete_head(Node* head) {
+  if (head == NULL) {
+    return NULL;
+  }
+  Node* tmp = head;
+  head = head->next;
+  delete tmp;
+  return head;
+}
