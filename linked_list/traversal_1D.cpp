52be419906391b25bca6b30b7dccb8539f1aacf4
Pranshu Gupta <pranshumg@gmail.com>
2025-11-03 13:24:10 +0530

    traversal of singly ll

--- linked_list/traversal_1D.cpp
+++ linked_list/traversal_1D.cpp
@@ -15,17 +15,6 @@ public:
   }
 };
 
-Node* convert_v_to_ll(vector<int>& v) {
-  Node* head = new Node(v[0]);
-  Node* mover = head;
-  for (int i = 1; i < (int)v.size(); ++i) {
-    Node* tmp = new Node(v[i]);
-    mover->next = tmp;
-    mover = tmp;
-  } 
-  return head;
-}
-
 // TC - O(n), SC - O(1)
 void traversal(Node* head) {
   Node* tmp = head;
@@ -33,10 +22,4 @@ void traversal(Node* head) {
     cout << tmp->data << ' ';
     tmp = tmp->next;
   }
-}
-
-int main() {
-  vector<int> v = {1, 3, 5, 7, 9};
-  Node* head = convert_v_to_ll(v);
-  traversal(head);
 }
