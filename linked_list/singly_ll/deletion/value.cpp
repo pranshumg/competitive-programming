9330f642b238b108b2d854d27241a08db4ab3966
Pranshu Gupta <pranshumg@gmail.com>
2025-11-05 20:15:51 +0530

    delete by node

---
+++ linked_list/singly_ll/deletion/value.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete node by value in a singly linked list */
+
+class Node {
+public:
+  int data;
+  Node* next;
+
+  Node(int val) {
+    data = val;
+    next = nullptr;
+  }
+};
+
+// TC - O(n), SC - O(1)
+Node* delete_val(Node* head, int el) {
+  if (head == nullptr) {
+    return head;
+  }
+  if (head->data == el) {
+    Node* tmp = head;
+    head = head->next;
+    delete tmp;
+    return head;
+  }
+  Node* tmp = head;
+  Node* prev = nullptr;
+  while (tmp != nullptr) {
+    if (tmp->data == el) {
+      prev->next = tmp->next;
+      delete tmp;
+      break;
+    }
+    prev = tmp;
+    tmp = tmp->next;
+  }
+  return head;
+}
