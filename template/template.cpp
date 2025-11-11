f5a574b8d05a5050674751bb786e6d9f90463128
Pranshu Gupta <pranshumg@gmail.com>
2025-11-11 17:53:29 +0530

    updated macros

--- template/template.cpp
+++ template/template.cpp
@@ -9,34 +9,31 @@ using ll = long long;
 using ld = long double;
 using ull = unsigned long long;
 
-using VI = vector<int>;
-using VL = vector<ll>;
-using VS = vector<string>;
-using VVI = vector<vector<int>>;
-using VVL = vector<vector<ll>>;
-using VPII = vector<pair<int, int>>;
-using VPLL = vector<pair<ll, ll>>;
-using PII = pair<int, int>;
-using PLL = pair<ll, ll>;
-using MII = map<int, int>;
-using MLL = map<ll, ll>;
-using MCI = map<char, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using vs = vector<string>;
+using vvi = vector<vector<int>>;
+using vvl = vector<vector<ll>>;
+using vpii = vector<pair<int, int>>;
+using vpll = vector<pair<ll, ll>>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using mii = map<int, int>;
+using mll = map<ll, ll>;
+using mci = map<char, int>;
 
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
-#define REP(i, a, b) for (int i = (a); i < (b); ++i)
-#define REPD(i, a, b) for (int i = (a); i >= (b); --i)
-#define EACH(x, a) for (auto &x : (a))
-#define SZ(x) (int)((x).size())
-#define PB push_back
-#define EB emplace_back
-#define PP pop_back
-#define MP make_pair
-#define F first
-#define S second
+#define sz(x) (int)((x).size())
+#define pb push_back
+#define eb emplace_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
 
 void run_case() {
-  ${0}
+  
 }
 
 int main() {
