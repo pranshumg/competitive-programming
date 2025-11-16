caff3900ec55987095807cbec6e7c003b563e07e
Pranshu Gupta <pranshumg@gmail.com>
2025-11-16 13:46:54 +0530

    delete head

---
+++ linked_list/doubly_ll/deletion/head.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Delete head of a doubly linked list */
+
+class Node {
+public:
+  int data;
+  Node* next;
+  Node* prev;
+
+  Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+  }
+
+  Node(int data, Node* next, Node* prev) {
+    this->data = data;
+    this->next = next;
+    this->prev = prev;
+  }
+};
+
+// TC - O(1), SC - O(1)
+Node* delete_head(Node* head) {
+  if (head == nullptr) {
+    return nullptr;
+  }
+  Node* tmp = head;
+  head = head->next;
+  head->prev = nullptr;
+  tmp->next = nullptr;
+  delete tmp;
+  return head;
+}
