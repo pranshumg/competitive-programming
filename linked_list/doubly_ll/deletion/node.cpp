b92d7979e381716414d31ea5b3a2567e32084ba4
Pranshu Gupta <pranshumg@gmail.com>
2025-11-16 14:54:10 +0530

    added delete node function

--- linked_list/doubly_ll/deletion/node.cpp
+++ linked_list/doubly_ll/deletion/node.cpp
@@ -23,3 +23,18 @@ public:
   }
 };
 
+// TC - O(1), SC - O(1)
+Node* delete_node(Node* tmp) {
+  Node* back = tmp->prev;
+  Node* front = tmp->next;
+  if (front == nullptr) {
+    back->next = nullptr;
+    tmp->prev = nullptr;
+    delete tmp;
+    return;
+  }
+  back->next = front;
+  front->prev = back;
+  tmp->prev = tmp->next = nullptr;
+  delete tmp;
+}
