e1570a47c39200dd58a820da8905098cd47ed8b3
Pranshu Gupta <pranshumg@gmail.com>
2025-11-17 22:30:20 +0530

    reverse a singly ll

---
+++ linked_list/singly_ll/reverse.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Reverse a singly linked list */
+
+class Node {
+public:
+  int data;
+  Node* next;
+
+  Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+// TC - O(n), SC - O(n)
+Node* reverse(Node* head) {
+  stack<int> sk;
+  Node* tmp = head;
+  while (tmp != nullptr) {
+    sk.push(tmp->data);
+    tmp = tmp->next;
+  }
+  tmp = head;
+  while (tmp != nullptr) {
+    tmp->data = sk.top();
+    sk.pop();
+    tmp = tmp->next;
+  }
+  return head;
+}
+
+// TC - O(n), SC - O(1)
+Node* reverse(Node* head) {
+  Node* tmp = head;
+  Node* back = nullptr;
+  while (tmp != nullptr) {
+    Node* front = tmp->next;
+    tmp->next = back;
+    back = tmp;
+    tmp = front;
+  }
+  return back;
+}
