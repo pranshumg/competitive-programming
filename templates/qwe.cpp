6dd97e068a1a60afa5501b98446be5518b4de0ad
Pranshu Gupta <pranshumg@gmail.com>
2025-11-27 16:30:55 +0530

    cpp template

---
+++ templates/qwe.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+inline int ri() {
+  int x;
+  cin >> x;
+  return x;
+}
+
+void run_case() {
+  ${0}
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tests = ri();
+  while (tests--) {
+    run_case();
+  }
+  return 0;
+}
