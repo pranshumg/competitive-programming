1786a01fc6d88a19614abd64e31029dd442b85b6
Pranshu Gupta <pranshumg@gmail.com>
2025-11-28 18:24:29 +0530

    cpp template

---
+++ templates/code.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+#define eb emplace_back
+#define ff first
+#define ss second
+#define all(x) (x).begin(), (x).end()
+
+void run_case() {
+  $0
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int tests;
+  cin >> tests;
+  while (tests--) {
+    run_case();
+  }
+  return 0;
+}
