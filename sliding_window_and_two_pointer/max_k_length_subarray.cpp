2eaac530a6260283042f4b964b65b4c371d14cdd
Pranshu Gupta <pranshumg@gmail.com>
2025-12-01 17:54:27 +0530

    maximum sum of a k length subarray

---
+++ sliding_window_and_two_pointer/max_k_length_subarray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Maximum sum of a k length subarray */
+
+// TC - O(n), SC - O(1)
+int max_sum(vector<int>& v, int k) {
+  int n = (int)v.size();
+  int sum = accumulate(v.begin(), v.begin() + k, 0);
+  int ans = sum, l = 0, r = k - 1;
+  while (r < n) {
+    sum -= v[l++];
+    sum += v[r++];
+    ans = max(ans, sum);
+  }
+  return ans;
+}
