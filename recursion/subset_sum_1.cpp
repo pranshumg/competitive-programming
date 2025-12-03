a938bb0babf86f55e7b3a3604de126d24ee2c170
Pranshu Gupta <pranshumg@gmail.com>
2025-12-03 22:47:44 +0530

    subset sum 1

--- recursion/subset_sum_1.cpp
+++ recursion/subset_sum_1.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 /* Subset sum 1 */
 
-// TC - (2^n log 2^n), SC - O(2^n)
 void generate(int idx, int sum, vector<int>& v, vector<int>& ans) {
   if (idx == (int)v.size()) {
     ans.emplace_back(sum);
@@ -14,6 +13,7 @@ void generate(int idx, int sum, vector<int>& v, vector<int>& ans) {
   generate(idx + 1, sum, v, ans);
 }
 
+// TC - (2^n log 2^n), SC - O(2^n)
 vector<int> subset_sum(vector<int>& v) {
   vector<int> ans;
   generate(0, 0, v, ans);
