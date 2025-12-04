5aaed2bd8485640cb136b813eff4db4f3ee0c55b
Pranshu Gupta <pranshumg@gmail.com>
2025-12-04 15:16:01 +0530

    updated tab size

--- templates/code.cpp
+++ templates/code.cpp
@@ -10,16 +10,16 @@ using u128 = unsigned __int128;
 #define all(x) (x).begin(), (x).end()
 
 void solve() {
-  $0
+    $0
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
-    solve();
-  }
-  return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
