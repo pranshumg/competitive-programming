bcd7f58a4ba83f4ec2bbbf9efcf0c51f6a36a7ae
Pranshu Gupta <pranshumg@gmail.com>
2025-12-06 01:59:10 +0530

    sieve of eratosthenes

---
+++ maths/sieve_of_eratosthenes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Sieve of Eratosthenes */
+
+// TC - O(n log log n), SC - O(n)
+vector<bool> sieve(int n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    return is_prime;
+}
