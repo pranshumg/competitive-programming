86085f4cb0534eff9f7d47f407ce96ce1f66bd4c
Pranshu Gupta <pranshumg@gmail.com>
2025-12-07 23:29:11 +0530

    left view

---
+++ trees/left_view.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Left view */
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        this->val = val;
+        left = right = nullptr;
+    }
+};
+
+void solve(Node* root, int level, vector<int>& ans) {
+    if (!root) {
+        return;
+    }
+    if (ans.size() == level) {
+        ans.emplace_back(root->val);
+    }
+    solve(root->left, level + 1, ans);
+    solve(root->right, level + 1, ans);
+}
+
+// TC - O(n), SC - O(n)
+vector<int> left_view(Node* root) {
+    vector<int> ans;
+    solve(root, 0, ans);
+    return ans;
+}
