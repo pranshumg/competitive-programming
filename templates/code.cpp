1422b1d7b4971451e5c215429286b548511b7012
Pranshu Gupta <pranshumg@gmail.com>
2025-12-19 14:33:35 +0530

    updated class

--- templates/code.cpp
+++ templates/code.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 using ll = long long;
 using ull = unsigned long long;
-using u32 = unsigned;
 using i128 = __int128;
 using u128 = unsigned __int128;
 
@@ -15,7 +14,7 @@ void solve() {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin >> t;
