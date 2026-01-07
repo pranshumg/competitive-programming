b8c4faf191de22baab52eb816093875f8983e81c
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 19:59:04 +0530

    GCD

--- maths/GCD.cpp
+++ maths/GCD.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 /* GCD */
+
 // TC - O(min(n, m))
-void gcd(int n, int m) {
-  for (int i = min(n, m); i >= 1; i--) {
-    if (n % i == 0 && m % i == 0) {
-      cout << i << '\n';
-      break;
+int gcd(int n, int m) {
+    for (int i = min(n, m); i >= 1; --i) {
+        if (n % i == 0 && m % i == 0) {
+            return i;
+            break;
+        }
     }
-  }
 }
