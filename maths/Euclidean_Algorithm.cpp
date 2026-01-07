9947d11790d407ce43150f08bf20bbd980988a9a
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 20:00:13 +0530

    eulidean algo

--- maths/Euclidean_Algorithm.cpp
+++ maths/Euclidean_Algorithm.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 /* Euclidean algorithm */
+
 // TC - O(log min(n, m))
 int euclidean_algorithm(int n, int m) {
-  while (n > 0 && m > 0) {
-    n > m ? n %= m : m %= n;
-  }
-  return (n == 0 ? m : n);
+    while (n > 0 && m > 0) {
+        n > m ? n %= m : m %= n;
+    }
+    return (n == 0 ? m : n);
 }
