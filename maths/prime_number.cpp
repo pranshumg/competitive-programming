bae77b913de5f02ca1cc3562cea4e6964ea9636f
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 19:56:44 +0530

    check prime number

--- maths/prime_number.cpp
+++ maths/prime_number.cpp
@@ -3,29 +3,28 @@
 using namespace std;
 
 /* Prime number */
-// Method 1
+
 // TC - O(n)
-void prime_number(int n) {
-  int cnt = 0;
-  for (int i = 1; i <= n; i++) {
-    if (n % i == 0) {
-      cnt++;
+bool is_prime(int n) {
+    int cnt = 0;
+    for (int i = 2; i < n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
     }
-  }
-  cout << (cnt == 2 ? "YES\n" : "NO\n");
+    return true;
 }
 
-// Method 2
-// TC - O(√n)
-void prime_number(int n) {
-  int cnt = 0;
-  for (int i = 1; i * i <= n; i++) {
-    if (n % i == 0) {
-      cnt++;
-      if (n / i != i) {
-        cnt++;
-      }
+// TC - O(sqrt(n))
+bool is_prime(int n) {
+    int cnt = 0;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ++cnt;
+            if (n / i != i) {
+                ++cnt;
+            }
+        }
     }
-  }
-  cout << (cnt == 2 ? "YES\n" : "NO\n");
+    return cnt == 2;
 }
