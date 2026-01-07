51afaf896b6cb3fabfb04f274b36a3b01545df3f
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 20:11:02 +0530

    prime factors

--- maths/prime_factors.cpp
+++ maths/prime_factors.cpp
@@ -4,50 +4,52 @@ using namespace std;
 
 /* Prime factors of a number */
 
-bool prime(int n) {
-  int cnt = 0;
-  for (int i = 1; i * i <= n; ++i) {
-    if (n % i == 0) {
-      ++cnt;
-      if (n / i != i) {
-        ++cnt;
-      }
+bool is_prime(int n) {
+    int cnt = 0;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ++cnt;
+            if (n / i != i) {
+                ++cnt;
+            }
+        }
     }
-  }
-  return cnt == 2;
+    return cnt == 2;
 }
 
 // TC - O(n), SC - O(k) where k is the no. of distinct prime factors
 vector<int> prime_factors(int n) {
-  vector<int> ans;
-  for (int i = 2;  i * i <= n; ++i) {
-    if (n % i == 0) {
-      if (prime(i)) {
-        ans.emplace_back(i);
-      }
-      if (n / i != i) {
-        if (prime(n / i)) {
-          ans.emplace_back(n / i);
+    vector<int> ans;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            if (is_prime(i)) {
+                ans.emplace_back(i);
+            }
+            if (n / i != i) {
+                if (is_prime(n / i)) {
+                    ans.emplace_back(n / i);
+                }
+            }
         }
-      }
     }
-  }
-  return ans;
+    return ans;
 }
 
-// TC - O(√n), SC - O(k) where k is the no. of distinct prime factors
+// TC - O(sqrt(n)), SC - O(k) where k is the no. of distinct prime factors
 vector<int> prime_factors(int n) {
-  vector<int> ans;
-  for (int i = 2; i * i <= n; ++i) {
-    if (n % i == 0) {
-      ans.emplace_back(i);
-      while (n % i == 0) {
-        n /= i;
-      }
+    vector<int> ans;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ans.emplace_back(i);
+            // Divide n by i repeatedly to remove this factor entirely
+            while (n % i == 0) {
+                n /= i;
+            }
+        }
     }
-  }
-  if (n > 1) {
-    ans.emplace_back(n);
-  }
-  return ans;
+    // If n > 1, the remaining part is a prime factor itself
+    if (n > 1) {
+        ans.emplace_back(n);
+    }
+    return ans;
 }
