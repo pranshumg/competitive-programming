1e933ccb24e5349945a47e6aa8a167332d49c118
Pranshu Gupta <pranshumg@gmail.com>
2026-01-08 02:54:11 +0530

    LCM

--- maths/LCM.cpp
+++ maths/LCM.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 /* Optimal LCM function using the relation: LCM(n, m) = (n * m) / GCD(n, m) */
+
 // TC - O(log(min(n, m))), SC - O(1)
 int gcd(int n, int m) {
-  while (n > 0 && m > 0) {
-    n > m ? n %= m : m %= n;
-  }
-  return (n == 0 ? m : n);
+    while (n > 0 && m > 0) {
+        n > m ? n %= m : m %= n;
+    }
+    return (n == 0 ? m : n);
 }
 
 int lcm(int n, int m) {
-  int64_t prod = (int64_t)n * m;
-  return prod / gcd(n, m);
+    // Cast to int64_t to prevent overflow during multiplication
+    int64_t prod = (int64_t)n * m;
+    return prod / gcd(n, m);
 }
