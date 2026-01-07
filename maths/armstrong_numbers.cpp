d86696426653408b367ebeb4f3735345edf62537
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 19:07:10 +0530

    check number is armstrong

--- maths/armstrong_numbers.cpp
+++ maths/armstrong_numbers.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 
-/* Armstrong numbers */
+/* Armstrong number */
+
 // TC - O(log n)
-void armstrong_numbers(int n) {
-  int cnt = (int)(log10(n) + 1), sum = 0, og_n = n;
-  while (n > 0) {
-    int d = n % 10;
-    sum += pow(d, cnt);
-    n /= 10;
-  }
-  cout << (sum == og_n ? "YES\n" : "NO\n");
+bool armstrong_number(int n) {
+    int cnt = (int)(log10(n) + 1), sum = 0, og = n;
+    while (n) {
+        int d = n % 10;
+        sum += pow(d, cnt);
+        n /= 10;
+    }
+    return og == sum;
 }
