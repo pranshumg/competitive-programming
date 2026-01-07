ed59c91551ebc2327125581ca3ac491a3b371074
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 19:15:02 +0530

    divisors of a number

--- maths/divisors.cpp
+++ maths/divisors.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 /* Divisors */
-// Method 1
+
 // TC - O(n)
-void divisors(int n) {
-  for (int i = 1; i <= n; i++) {
-    if (n % i == 0) {
-      cout << i << ' ';
+vector<int> divisors(int n) {
+    vector<int> ans;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            ans.emplace_back(i);
+        }
     }
-  }
-  cout << '\n';
+    return ans;
 }
 
-// Method 2
-// TC - O(√n log n)
-void divisors(int n) {
-  vector<int> v;
-  for (int i = 1; i * i <= n; i++) {
-    if (n % i == 0) {
-      v.push_back(i);
-      if (n / i != i) {
-        v.push_back(n / i);
-      }
+// TC - O(sqrt(n))
+vector<int> divisors(int n) {
+    vector<int> ans;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ans.emplace_back(i);
+            // If the pair divisor is different, push it too
+            if (n / i != i) {
+                ans.emplace_back(n / i);
+            }
+        }
     }
-  }
-  sort(v.begin(), v.end());
-  for (auto it: v) {
-    cout << it << ' ';
-  }
-  cout << '\n';
+    return ans;
 }
