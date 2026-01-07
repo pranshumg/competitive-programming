7defe615a0b1391ce0a8d1c575cf916efd8c89e7
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 20:17:05 +0530

    power exponentiation

--- maths/power_exponentiation.cpp
+++ maths/power_exponentiation.cpp
@@ -6,17 +6,20 @@ using namespace std;
 
 // TC - O(log n), SC - O(1)
 double pow(double x, int n) {
-  double ans = 1;
-  int64_t m = abs((int64_t)n);
-  while (m > 0) {
-    if (m & 1) {
-      ans *= x, --m;
-    } else {
-      x *= x, m >>= 1;
+    double ans = 1.0;
+    // Use int64_t to safely handle n = INT_MIN because abs(INT_MIN) overflows int
+    int64_t m = abs((int64_t)n);
+    while (m > 0) {
+        if (m & 1) {
+            ans *= x;
+            --m;
+        } else {
+            x *= x;
+            m >>= 1;
+        }
     }
-  }
-  if (n < 0) {
-    ans = 1.0 / ans;
-  }
-  return ans;
+    if (n < 0) {
+        ans = 1.0 / ans;
+    }
+    return ans;
 }
