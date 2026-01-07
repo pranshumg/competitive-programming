729f7c7d088c753c28c489931745a555a97c8a6e
Pranshu Gupta <pranshumg@gmail.com>
2026-01-08 02:47:21 +0530

    smallest prime factor

--- maths/SPF.cpp
+++ maths/SPF.cpp
@@ -2,33 +2,40 @@
 
 using namespace std;
 
-/* Smallest Prime Factor */
+/* Smallest Prime Factor (SPF) using Sieve */
 
-// TC - (n log log n), SC - O(n)
+// TC - O(n log log n), SC - O(n)
 vector<int> SPF(int n) {
-  vector<int> ans(n + 1);
-  for (int i = 0; i < (int)ans.size(); ++i) {
-    ans[i] = i;
-  }
-  for (int i = 2; i * i <= n; ++i) {
-    if (ans[i] == i) {
-      for (int j = i * i; j <= n; j += i) {
-        if (ans[j] == j) {
-          ans[j] = i;
-        }
-      }
+    vector<int> ans(n + 1);
+    // Initialize SPF[i] = i
+    for (int i = 0; i < (int)ans.size(); ++i) {
+        ans[i] = i;
     }
-  }
-  return ans;
+    // Iterate from 2 to sqrt(n)
+    for (int i = 2; i * i <= n; ++i) {
+        // If ans[i] == i, then i is prime
+        if (ans[i] == i) {
+            // Mark SPF for all multiples of i
+            for (int j = i * i; j <= n; j += i) {
+                // Only update if it hasn't been touched yet (ans[j] == j)
+                // This ensures we record the *smallest* prime factor
+                if (ans[j] == j) {
+                    ans[j] = i;
+                }
+            }
+        }
+    }
+    return ans;
 }
 
 // TC - O(log n), SC - O(n)
 vector<int> prime_factors(int n) {
-  vector<int> ans;
-  vector<int> smallest_prime_factors = SPF(n);
-  while (n > 1) {
-    ans.emplace_back(smallest_prime_factors[n]);
-    n /= smallest_prime_factors[n];
-  }
-  return ans;
+    vector<int> ans;
+    // Calculate SPF table up to n
+    vector<int> spf = SPF(n);
+    while (n > 1) {
+        ans.emplace_back(spf[n]);
+        n /= spf[n];
+    }
+    return ans;
 }
