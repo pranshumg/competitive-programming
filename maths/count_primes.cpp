4a2a26c8bcb2bfbc65c36239b1248cb88eb5e386
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 20:41:10 +0530

    count primes in a range

--- maths/count_primes.cpp
+++ maths/count_primes.cpp
@@ -5,61 +5,73 @@ using namespace std;
 /* Count primes in a range L - R */
 
 bool is_prime(int n) {
-  int cnt = 0;
-  for (int i = 1; i * i <= n; ++i) {
-    if (!(n % i)) {
-      ++cnt;
-      if (n / i != i) {
-        ++cnt;
-      }
+    int cnt = 0;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            ++cnt;
+            if (n / i != i) {
+                ++cnt;
+            }
+        }
     }
-  }
-  return cnt == 2;
+    return cnt == 2;
 }
 
-vector<int> sieve(int n) {
-  vector<int> is_prime(n + 1, 1);
-  is_prime[0] = is_prime[1] = 0;
-  for (int i = 2; i * i <= n; ++i) {
-    if (is_prime[i]) {
-      for (int j = i * i; j <= n; j += i) {
-        is_prime[j] = 0;
-      }
+// Sieve of Eratosthenes
+vector<bool> sieve(int n) {
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; ++i) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                is_prime[j] = false;
+            }
+        }
     }
-  }
-  return is_prime;
+    return is_prime;
 }
 
-// TC - O((R - L + 1) * √n), SC - O(1);
+// Method 1: Naive Approach
+// TC - O((R - L + 1) * √n), SC - O(1)
 int count_primes(int L, int R) {
-  int cnt = 0;
-  for (int i = L; i <= R; ++i) {
-    if (is_prime(i)) {
-      ++cnt;
+    int cnt = 0;
+    for (int i = L; i <= R; ++i) {
+        if (is_prime(i)) {
+            ++cnt;
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
 
-// TC - O(1e6 log log 1e6 + (R - L + 1)), SC - O(1e6)
+// Method 2: Sieve (Direct Check)
+// TC - O(N log log N + (R - L)), SC - O(N) where N = 1e6
 int count_primes(int L, int R) {
-  vector<int> primes = sieve((int)1e6);
-  int cnt = 0;
-  for (int i = L; i <= R; ++i) {
-    if (primes[i]) {
-      ++cnt;
+    // Generate sieve up to the max possible value (e.g., 10^6)
+    vector<bool> is_prime = sieve((int)1e6);
+    int cnt = 0;
+    for (int i = L; i <= R; ++i) {
+        if (is_prime[i]) {
+            ++cnt;
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
 
-// TC - (1e6 log log 1e6 + 1e6), SC - O(1e6)
+// Method 3: Sieve with Prefix Sums (Optimal for multiple queries)
+// TC - O(N log log N + N) for precomputation, O(1) per query
 int count_primes(int L, int R) {
-  vector<int> primes = sieve((int)1e6);
-  int cnt = 0;
-  for (int i = 0; i < (int)primes.size(); ++i) {
-    cnt += primes[i];
-    primes[i] = cnt;
-  }
-  return primes[R] - primes[L - 1];
+    // 1. Get Boolean Sieve
+    vector<bool> is_prime = sieve((int)1e6);
+    // 2. Convert to Prefix Sum Array
+    // primes[i] will store the count of prime numbers from 0 to i
+    vector<int> primes((int)1e6 + 1);
+    int cnt = 0;
+    for (int i = 0; i < (int)primes.size(); ++i) {
+        if (is_prime[i]) {
+            cnt++;
+        }
+        primes[i] = cnt;
+    }
+    // 3. Answer Query in O(1)
+    return primes[R] - primes[L - 1];
 }
