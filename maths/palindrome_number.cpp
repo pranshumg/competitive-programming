eecbdb4c0e10a99861267e3ea26de2ad9c46abd3
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 19:04:25 +0530

    check number is palindrome

--- maths/palindrome_number.cpp
+++ maths/palindrome_number.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 /* Palindrome number */
+
 // TC - O(log n)
-void palindrome_number(int n) {
-  int rev_n = 0, og_n = n;
-  while (n > 0) {
-    int d = n % 10;
-    rev_n = rev_n * 10 + d;
-    n /= 10;
-  }
-  cout << (og_n == rev_n ? "YES\n" : "NO\n");
+bool palindrome_number(int n) {
+    int rev = 0, og = n;
+    while (n) {
+        int d = n % 10;
+        rev = rev * 10 + d;
+        n /= 10;
+    }
+    return og == rev;
 }
