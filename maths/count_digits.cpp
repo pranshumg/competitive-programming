d65e3eb6ffed03726b5f33ceb3f63e951b08cb83
Pranshu Gupta <pranshumg@gmail.com>
2026-01-07 18:53:21 +0530

    Count digits

--- maths/count_digits.cpp
+++ maths/count_digits.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 /* Count digits */
-// Method 1 
+
 // TC - O(log n)
 int count_digits(int n) {
-  int cnt = 0;
-  while (n > 0) {
-    cnt++, n /= 10;
-  }
-  return cnt;
+    int cnt = 0;
+    while (n > 0) {
+        ++cnt, n /= 10;
+    }
+    return cnt;
 }
 
-// Method 2
 // TC - O(1)
 int count_digits(int n) {
-  return (int)(log10(n) + 1);
+    return (int)(log10(n) + 1);
 }
