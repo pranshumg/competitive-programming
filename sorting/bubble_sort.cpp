eb6ab2950deb3ddb06a664974f2a82d1c4d53a4a
Pranshu Gupta <pranshumg@gmail.com>
2026-01-10 17:18:25 +0530

    Bubble sort

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,19 +2,20 @@
 
 using namespace std;
 
-// Best TC - O(n)
-// Worst TC - O(n²)
+/* Bubble sort */
+
+// TC - O(n * n), SC - O(1)
 void bubble_sort(vector<int>& v, int n) {
-  for (int i = 0; i < n; i++) {
-    int did_swap = 0;
-    for (int j = 0; j < n - 1 - i; j++) {
-      if (v[j] > v[j + 1]) {
-        swap(v[j], v[j + 1]);
-        did_swap = 1;
-      }
+    for (int i = 0; i < n; ++i) {
+        bool did_swap = false;
+        for (int j = 0; j < n - 1 - i; ++j) {
+            if (v[j] > v[j + 1]) {
+                swap(v[j], v[j + 1]);
+                did_swap = true;
+            }
+        }
+        if (!did_swap) {
+            break;
+        }
     }
-    if (!did_swap) {
-      break;
-    }
-  }
 }
