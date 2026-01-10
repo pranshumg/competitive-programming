6eec4d243608fd7751807101955fa942edf2b641
Pranshu Gupta <pranshumg@gmail.com>
2026-01-10 21:48:45 +0530

    quick sort

--- sorting/quick_sort.cpp
+++ sorting/quick_sort.cpp
@@ -2,31 +2,31 @@
 
 using namespace std;
 
-// TC - O(n log n)
-// SC - O(log n)
-int partition(vector<int>& v, int low, int high) {
-  int pivot = v[low], i = low, j = high;
-  while (i < j) {
-    while (v[i] <= pivot && i <= high) {
-      i++;
+/* Quick sort */
+
+// TC - O(n log n), SC - O(log n)
+int partition(vector<int>& v, int l, int h) {
+    int pivot = v[l], i = l, j = h;
+    while (i < j) {
+        while (v[i] <= pivot && i <= h) {
+            ++i;
+        }
+        while (v[j] > pivot && j >= l) {
+            --j;
+        }
+        if (i < j) {
+            swap(v[i], v[j]);
+        }
     }
-    while (v[j] > pivot && j >= low) {
-      j--;
-    }
-    if (i < j) {
-      swap(v[i], v[j]);
-    }
-  }
-  swap(v[low], v[j]);
-  return j;
+    swap(v[l], v[j]);
+    return j;
 }
 
-void quick_sort(vector<int>& v, int low, int high) {
-  if (low >= high) {
-    return ;
-  }
-  int pivot_idx = partition(v, low, high);
-  quick_sort(v, low, pivot_idx - 1);
-  quick_sort(v, pivot_idx + 1, high);
+void quick_sort(vector<int>& v, int l, int h) {
+    if (l >= h) {
+        return;
+    }
+    int pivot_idx = partition(v, l, h);
+    quick_sort(v, l, pivot_idx - 1);
+    quick_sort(v, pivot_idx + 1, h);
 }
-
