59e1cb7d38bf97c2ef4117ac0041cca881297830
Pranshu Gupta <pranshumg@gmail.com>
2026-01-10 17:36:53 +0530

    insertion sort

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -2,13 +2,14 @@
 
 using namespace std;
 
-// Best TC - O(n)
-// Worst TC - O(n²)
+/* Insertion sort */
+
+// TC - O(n * n), SC - O(1)
 void insertion_sort(vector<int>& v, int n) {
-  for (int i = 1; i < n; i++) {
-    int j = i;
-    while (j > 0 && v[j - 1] > v[j]) {
-      swap(v[j - 1], v[j--]);
+    for (int i = 1; i < n; ++i) {
+        int j = i;
+        while (j > 0 && v[j - 1] > v[j]) {
+            swap(v[j - 1], v[j--]);
+        }
     }
-  }
 }
