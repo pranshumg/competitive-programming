2e39cc78593cb19fc851020841deac7965c8253b
Pranshu Gupta <pranshumg@gmail.com>
2026-01-12 23:12:04 +0530

    insertion sort recursion

--- sorting/insertion_sort_recursion.cpp
+++ sorting/insertion_sort_recursion.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-// Best TC - O(n)
-// Worst TC - O(n²)
-void insertion_sort_rec(vector<int>& v, int i, int n) {
-  if (i == n) {
-    return;
-  }
-  int j = i;
-  while (j > 0 && v[j - 1] > v[j]) {
-    swap(v[j - 1], v[j--]);
-  }
-  insertion_sort_rec(v, i + 1, n);
-}
+/* Insertion sort (recursion) */
 
+// TC - O(n * n), SC - O(n)
+void insertion_sort_rec(vector<int>& v, int i, int n) {
+    if (i == n) {
+        return;
+    }
+    int j = i;
+    while (j > 0 && v[j - 1] > v[j]) {
+        swap(v[j - 1], v[j--]);
+    }
+    insertion_sort_rec(v, i + 1, n);
+}
