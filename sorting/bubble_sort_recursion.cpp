6653d606320543bd5bd944c239a5df09c0c25172
Pranshu Gupta <pranshumg@gmail.com>
2026-01-12 23:10:30 +0530

    bubble sort recursion

--- sorting/bubble_sort_recursion.cpp
+++ sorting/bubble_sort_recursion.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-// Best TC - O(n)
-// Worst TC - O(n²)
-void bubble_sort_rec(vector<int>& v, int i, int n) {
-  if (i == n - 1) {
-    return;
-  }
-  int did_swap = 0;
-  for (int j = 0; j < n - 1 - i; j++) {
-    if (v[j] > v[j + 1]) {
-      swap(v[j], v[j + 1]);
-      did_swap = 1;
-    }
-  }
-  if (!did_swap) {
-    return;
-  }
-  bubble_sort_rec(v, i + 1, n);
-}
+/* Bubble sort (recursion) */
 
+// TC - O(n * n), SC - O(n)
+void bubble_sort_rec(vector<int>& v, int i, int n) {
+    if (i == n - 1) {
+        return;
+    }
+    int did_swap = 0;
+    for (int j = 0; j < n - 1 - i; j++) {
+        if (v[j] > v[j + 1]) {
+            swap(v[j], v[j + 1]);
+            did_swap = 1;
+        }
+    }
+    if (!did_swap) {
+        return;
+    }
+    bubble_sort_rec(v, i + 1, n);
+}
