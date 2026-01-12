0c53f17d08b009e1523c1347142bbdd93163104f
Pranshu Gupta <pranshumg@gmail.com>
2026-01-12 23:13:25 +0530

    selection sort recursion

--- sorting/selection_sort_recursion.cpp
+++ sorting/selection_sort_recursion.cpp
@@ -2,18 +2,19 @@
 
 using namespace std;
 
-// TC - O(n²)
-void selection_sort_rec(vector<int>& v, int i, int n) {
-  if (i == n - 1) {
-    return;
-  }
-  int mini_idx = i;
-  for (int j = i + 1; j < n; j++) {
-    if (v[j] < v[mini_idx]) {
-      mini_idx = j;
-    }
-  }
-  swap(v[i], v[mini_idx]);
-  selection_sort_rec(v, i + 1, n);
-}
+/* Selection sort (recursion) */
 
+// TC - O(n * n), SC - O(n)
+void selection_sort_rec(vector<int>& v, int i, int n) {
+    if (i == n - 1) {
+        return;
+    }
+    int mini_idx = i;
+    for (int j = i + 1; j < n; j++) {
+        if (v[j] < v[mini_idx]) {
+            mini_idx = j;
+        }
+    }
+    swap(v[i], v[mini_idx]);
+    selection_sort_rec(v, i + 1, n);
+}
