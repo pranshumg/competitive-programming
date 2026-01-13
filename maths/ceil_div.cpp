eb41f4c2219cedfb16b753b97315448b9d393d50
Pranshu Gupta <pranshumg@gmail.com>
2026-01-13 12:07:05 +0530

    changed long long to int64_t

--- maths/ceil_div.cpp
+++ maths/ceil_div.cpp
@@ -5,6 +5,6 @@ using namespace std;
 /* Ceil division */
 
 // TC - O(1), SC - O(1)
-long long ceil_div(long long a, long long b) {
+int64_t ceil_div(int64_t a, int64_t b) {
   return (a + b - 1) / b;
 }
