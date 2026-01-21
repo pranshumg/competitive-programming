3e51b8a5db3ca2289c3bda56152db7e478533c21
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 01:46:20 +0530

    rotate array by one place

--- arrays/rotate_by_one_place.cpp
+++ arrays/rotate_by_one_place.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 /* Rotate array by one place */
+
 // TC - O(n), SC - O(1)
 void rotate_left_by_one_place(vector<int>& v, int n) {
-  int temp = v[0];
-  for (int i = 1; i < n; i++) {
-    v[i - 1] = v[i];
-  }
-  v[n - 1] = temp;
+    int tmp = v[0];
+    for (int i = 1; i < n; ++i) {
+        v[i - 1] = v[i];
+    }
+    v[n - 1] = tmp;
 }
 
 // TC - O(n), SC - O(1)
 void rotate_right_by_one_place(vector<int>& v, int n) {
-  int temp = v[n - 1];
-  for (int i = n - 1; i > 0; i--) {
-    v[i] = v[i - 1];
-  }
-  v[0] = temp;
+    int tmp = v[n - 1];
+    for (int i = n - 1; i > 0; i--) {
+        v[i] = v[i - 1];
+    }
+    v[0] = tmp;
 }
