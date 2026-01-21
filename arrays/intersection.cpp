e75ab8d18ac3f1fea55a1822bdfc0767a402ca57
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 02:26:32 +0530

    intersection of two sorted arrays

--- arrays/intersection.cpp
+++ arrays/intersection.cpp
@@ -3,38 +3,42 @@
 using namespace std;
 
 /* Intersection of two sorted arrays */
-// Brute (TC - O(n * m), SC - O(min(n, m)))
+
+// Brute 
+// TC - O(n * m), SC - O(m)
 vector<int> intersection(vector<int>& v1, vector<int>& v2) {
-  int n = (int)v1.size(), m = (int)v2.size();
-  vector<int> ans, visited(min(n, m));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (v1[i] == v2[j] && visited[j] == 0) {
-        ans.push_back(v1[i]);
-        visited[j] = 1;
-        break;
-      }
-      if (v2[j] > v1[i]) {
-        break;
-      }
+    int n = (int)v1.size(), m = (int)v2.size();
+    vector<int> ans, vis(m, 0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (v1[i] == v2[j] && !vis[j]) {
+                ans.emplace_back(v1[i]);
+                vis[j] = 1;
+                break;
+            }
+            if (v2[j] > v1[i]) {
+                break;
+            }
+        }
     }
-  }
-  return ans;
+    return ans;
 }
 
-// Optimal (TC - O(n + m), SC - O(1))
+// Optimal
+// TC - O(n + m), SC - O(1)
 vector<int> intersection(vector<int>& v1, vector<int>& v2) {
-  int n = (int)v1.size(), m = (int)v2.size(), i = 0, j = 0;
-  vector<int> ans;
-  while (i < n && j < m) {
-    if (v1[i] > v2[j]) {
-      j++;
-    } else if (v1[i] < v2[j]) {
-      i++;
-    } else {
-      ans.push_back(v1[i]);
-      i++, j++;
+    int n = (int)v1.size(), m = (int)v2.size();
+    int i = 0, j = 0;
+    vector<int> ans;
+    while (i < n && j < m) {
+        if (v1[i] > v2[j]) {
+            ++j;
+        } else if (v1[i] < v2[j]) {
+            ++i;
+        } else {
+            ans.emplace_back(v1[i++]);
+            ++j;
+        }
     }
-  }
-  return ans;
+    return ans;
 }
