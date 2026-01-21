73f8c647f4735cf300a8f3e987d9cde7a8a04625
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 01:42:33 +0530

    Remove duplicates from sorted array

--- arrays/remove_duplicates.cpp
+++ arrays/remove_duplicates.cpp
@@ -3,27 +3,28 @@
 using namespace std;
 
 /* Remove Duplicates from sorted array */
-// Brute (TC - O(n log n), SC - O(n))
+
+// (TC - O(n log n), SC - O(n))
 int remove_duplicates(vector<int>& v, int n) {
-  set<int> st;
-  for (int i = 0; i < n; i++) {
-    st.insert(v[i]);
-  }
-  int idx = 0;
-  for (auto it : st) {
-    v[idx++] = it;
-  }
-  return st.size();
+    set<int> st;
+    for (int i = 0; i < n; ++i) {
+        st.insert(v[i]);
+    }
+    int idx = 0;
+    for (auto it : st) {
+        v[idx++] = it;
+    }
+    return (int)st.size();
 }
 
 // Optimal (TC - O(n), SC - O(1))
-int remove_duplicates(vector<int>& v, int n) {
-  int i = 0, j = 1;
-  while (j < n) {
-    if (v[i] != v[j]) {
-      v[i++ + 1] = v[j];
+int remove_duplicates_optimal(vector<int>& v, int n) {
+    int i = 0, j = 1;
+    while (j < n) {
+        if (v[i] != v[j]) {
+            v[i++ + 1] = v[j];
+        }
+        j++;
     }
-    j++;
-  }
-  return i + 1;
+    return i + 1;
 }
