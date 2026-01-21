be8046fe151c1fc6ae5ce866681297dfd83a286f
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 02:43:52 +0530

    largest and smallest element

--- arrays/largest_and_smallest_element.cpp
+++ arrays/largest_and_smallest_element.cpp
@@ -3,37 +3,39 @@
 using namespace std;
 
 /* Largest element */
-// Brute (TC - O(n log n), SC - O(1))
+
+// Brute 
+// TC - O(n log n), SC - O(1)
 int largest_element(vector<int>& v, int n) {
-  sort(v.begin(), v.end());
-  return v[n - 1];
+    sort(v.begin(), v.end());
+    return v[n - 1];
 }
 
-// Optimal (TC - O(n), SC - O(1))
+// Optimal 
+// TC - O(n), SC - O(1)
 int largest_element(vector<int>& v, int n) {
-  int largest = v[0];
-  for (int i = 1; i < n; i++) {
-    if (largest < v[i]) {
-      largest = v[i];
+    int largest = v[0];
+    for (int i = 1; i < n; ++i) {
+        largest = max(largest, v[i]);
     }
-  }
-  return largest;
+    return largest;
 }
 
 /* Smallest element */
-// Brute (TC - O(n log n), SC - O(1))
+
+// Brute 
+// TC - O(n log n), SC - O(1)
 int smallest_element(vector<int>& v, int n) {
-  sort(v.begin(), v.end());
-  return v[0];
+    sort(v.begin(), v.end());
+    return v[0];
 }
 
-// Optimal (TC - O(n), SC - O(1))
+// Optimal
+// TC - O(n), SC - O(1)
 int smallest_element(vector<int>& v, int n) {
-  int smallest = v[0];
-  for (int i = 1; i < n; i++) {
-    if (smallest > v[i]) {
-      smallest = v[i];
+    int smallest = v[0];
+    for (int i = 1; i < n; ++i) {
+        smallest = min(smallest, v[i]);
     }
-  }
-  return smallest;
+    return smallest;
 }
