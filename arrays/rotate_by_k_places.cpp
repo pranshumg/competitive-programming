328153a09c90458180201fd3b4883e828a2d33c4
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 01:58:08 +0530

    rotate array by k places

--- arrays/rotate_by_k_places.cpp
+++ arrays/rotate_by_k_places.cpp
@@ -3,42 +3,47 @@
 using namespace std;
 
 /* Rotate array by k places */
-// Brute (TC - O(n), SC - O(k))
+
+// Brute
+// TC - O(n), SC - O(k)
 void rotate_left_by_k_places(vector<int>& v, int n, int k) {
-  k %= n;
-  vector<int> temp(v.begin(), v.begin() + k);
-  for (int i = k; i < n; i++) {
-    v[i - k] = v[i];
-  }
-  for (int i = n - k; i < n; i++) {
-    v[i] = temp[i - (n - k)];
-  }
+    k %= n;
+    vector<int> tmp(v.begin(), v.begin() + k);
+    for (int i = k; i < n; ++i) {
+        v[i - k] = v[i];
+    }
+    for (int i = n - k; i < n; ++i) {
+        v[i] = tmp[i - n - k];
+    }
 }
 
-// Optimal (TC - O(n), SC - O(1))
+// Optimal
+// TC - O(n), SC - O(1)
 void rotate_left_by_k_places(vector<int>& v, int n, int k) {
-  k %= n;
-  reverse(v.begin(), v.begin() + k);
-  reverse(v.begin() + k, v.end());
-  reverse(v.begin(), v.end());
+    k %= n;
+    reverse(v.begin(), v.begin() + k);
+    reverse(v.begin() + k, v.end());
+    reverse(v.begin(), v.end());
 }
 
-// Brute (TC - O(n), SC - O(k))
+// Brute
+// TC - O(n), SC - O(k)
 void rotate_right_by_k_places(vector<int>& v, int n, int k) {
-  k %= n;
-  vector<int> temp(v.begin(), v.begin() + (n - k));
-  for (int i = n - k; i < n; i++) {
-    v[i - (n - k)] = v[i];
-  }
-  for (int i = k; i < n; i++) {
-    v[i] = temp[i - k];
-  }
+    k %= n;
+    vector<int> tmp(v.begin(), v.begin() + (n - k));
+    for (int i = n - k; i < n; ++i) {
+        v[i - (n - k)] = v[i];
+    }
+    for (int i = k; i < n; ++i) {
+        v[i] = tmp[i - k];
+    }
 }
 
-// Optimal (TC - O(n), SC - O(1))
+// Optimal 
+// TC - O(n), SC - O(1)
 void rotate_right_by_k_places(vector<int>& v, int n, int k) {
-  k %= n;
-  reverse(v.begin(), v.begin() + (n - k));
-  reverse(v.begin() + (n - k), v.end());
-  reverse(v.begin(), v.end());
+    k %= n;
+    reverse(v.begin(), v.begin() + (n - k));
+    reverse(v.begin() + (n - k), v.end());
+    reverse(v.begin(), v.end());
 }
