288fbb45db9b250810e491633951abda9174f0b1
Pranshu Gupta <pranshumg@gmail.com>
2026-01-21 22:29:20 +0530

    to check if the array is sorted in non-decreasing order

--- arrays/is_sorted.cpp
+++ arrays/is_sorted.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 /* To check array is sorted in non-decreasing order */
+
 // TC - O(n), SC - O(1)
 bool is_sorted(vector<int>& v, int n) {
-  for (int i = 0; i < n - 1; i++) {
-    if (v[i] > v[i + 1]) {
-      return false;
+    for (int i = 0; i < n - 1; ++i) {
+        if (v[i] > v[i + 1]) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
