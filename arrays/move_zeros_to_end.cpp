a8510e3379fb49f351719a4d1fc8051001295efb
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 02:32:37 +0530

    move zeroes to end

--- arrays/move_zeros_to_end.cpp
+++ arrays/move_zeros_to_end.cpp
@@ -3,37 +3,40 @@
 using namespace std;
 
 /* Move zeros to end */
-// Brute (TC - O(n), SC - O(n))
+
+// Brute 
+// TC - O(n), SC - O(n)
 void move_zeros_to_end(vector<int>& v, int n) {
-  vector<int> temp;
-  for (int i = 0; i < n; i++) {
-    if (v[i] != 0) {
-      temp.push_back(v[i]);
+    vector<int> tmp;
+    for (int i = 0; i < n; ++i) {
+        if (v[i]) {
+            tmp.emplace_back(v[i]);
+        }
     }
-  }
-  for (int i = 0; i < (int)temp.size(); i++) {
-    v[i] = temp[i];
-  }
-  for (int i = (int)temp.size(); i < n; i++) {
-    v[i] = 0;
-  }
+    for (int i = 0; i < (int)tmp.size(); ++i) {
+        v[i] = tmp[i];
+    }
+    for (int i = (int)tmp.size(); i < n; ++i) {
+        v[i] = 0;
+    }
 }
 
-// Optimal (TC - O(n), SC - O(1))
+// Optimal 
+// TC - O(n), SC - O(1)
 void move_zeros_to_end(vector<int>& v, int n) {
-  int j = -1;
-  for (int i = 0; i < n; i++) {
-    if (v[i] == 0) {
-      j = i;
-      break;
+    int j = -1;
+    for (int i = 0; i < n; ++i) {
+        if (!v[i]) {
+            j = i;
+            break;
+        }
     }
-  }
-  if (j == -1) {
-    return;
-  }
-  for (int i = j + 1; i < n; i++) {
-    if (v[i] != 0) {
-      swap(v[i], v[j++]);
+    if (j == -1) {
+        return;
     }
-  }
+    for (int i = j + 1; i < n; ++i) {
+        if (v[i]) {
+            swap(v[i], v[j++]);
+        }
+    }
 }
