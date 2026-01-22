186febfc78e90fc222ae11bf4a856f6a896da3fe
Pranshu Gupta <pranshumg@gmail.com>
2026-01-22 22:55:38 +0530

    longest subarray with sum k of pos and neg elements

--- arrays/longest_subarray_with_sum_k_pos_and_neg.cpp
+++ arrays/longest_subarray_with_sum_k_pos_and_neg.cpp
@@ -3,36 +3,39 @@
 using namespace std;
 
 /* Longest subarray with sum k (pos and neg) */
-// Brute (TC - O(n²), SC - O(1))
+
+// Brute 
+// TC - O(n * n), SC - O(1)
 int longest_subarray(vector<int>& v, int n, int k) {
-  int len = 0;
-  for (int i = 0; i < n; i++) {
-    int sum = 0;
-    for (int j = i; j < n; j++) {
-      sum += v[i];
-      if (sum == k) {
-        len = max(len, j - i + 1);
-      }
+    int len = 0;
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += v[j];
+            if (sum == k) {
+                len = max(len, j - i + 1);
+            }
+        }
     }
-  }
-  return len;
+    return len;
 }
 
-// Optimal (TC - O(n), SC - O(n))
+// Optimal 
+// TC - O(n), SC - O(n)
 int longest_subarray(vector<int>& v, int n, int k) {
-  unordered_map<int, int> pre_sum;
-  int len = 0, sum = 0;
-  for (int i = 0; i < n; i++) {
-    sum += v[i];
-    if (sum == k) {
-      len = max(len, i + 1);
+    map<int, int> pre;
+    int len = 0, sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += v[i];
+        if (sum == k) {
+            len = max(len, i + 1);
+        }
+        if (pre.find(sum - k) != pre.end()) {
+            len = max(len, i - pre[sum - k]);
+        }
+        if (pre.find(sum) == pre.end()) {
+            pre[sum] = i;
+        }
     }
-    if (pre_sum.find(sum - k) != pre_sum.end()) {
-      len = max(len, i - pre_sum[sum - k]);
-    }
-    if (pre_sum.find(sum) == pre_sum.end()) {
-      pre_sum[sum] = i;
-    }
-  }
-  return len;
+    return len;
 }
