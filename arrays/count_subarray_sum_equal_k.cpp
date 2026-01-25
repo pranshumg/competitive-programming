63d59338e0110349660d515698bab430bc4b9fda
Pranshu Gupta <pranshumg@gmail.com>
2026-01-25 16:50:34 +0530

    count subarrays with sum equal to k

--- arrays/count_subarray_sum_equal_k.cpp
+++ arrays/count_subarray_sum_equal_k.cpp
@@ -3,47 +3,51 @@
 using namespace std;
 
 /* Count subarray sum equal k */
-// Brute (TC - O(n3), SC - O(1))
-int subarray_sum(vector<int>& v, int n, int k) {
-  int cnt = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
-      int sum = 0;
-        for (int l = i; l <= j; l++) {
-          sum += v[l];
-        }
-        if (sum == k) {
-          cnt++;
-        }
-      }
-  }
-  return cnt;
-}
 
-// Better (TC - O(n2), SC - O(1))
+// Brute 
+// TC - O(n * n * n), SC - O(1)
 int subarray_sum(vector<int>& v, int n, int k) {
-  int cnt = 0;
-  for (int i = 0; i < n; i++) {
-    int sum = 0;
-    for (int j = i; j < n; j++) {
-      sum += v[j];
-      if (sum == k) {
-        cnt++;
-      }
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            int sum = 0;
+            for (int l = i; l <= j; ++l) {
+                sum += v[l];
+            }
+            if (sum == k) {
+                ++cnt;
+            }
+        }
     }
-  }
-  return cnt;
+    return cnt;
 }
 
-// Optimal (TC - O(n), SC - O(n))
+// Better 
+// TC - O(n * n), SC - O(1)
 int subarray_sum(vector<int>& v, int n, int k) {
-  int cnt = 0, sum = 0;
-  unordered_map<int, int> pre_sum;
-  pre_sum[0]++;
-  for (int i = 0; i < n; i++) {
-    sum += v[i];
-    cnt += pre_sum[sum - k];
-    pre_sum[sum]++;
-  }
-  return cnt;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += v[j];
+            if (sum == k) {
+                ++cnt;
+            }
+        }
+    }
+    return cnt;
+}
+
+// Optimal 
+// TC - O(n), SC - O(n)
+int subarray_sum(vector<int>& v, int n, int k) {
+    int cnt = 0, sum = 0;
+    map<int, int> pre;
+    ++pre[0];
+    for (int i = 0; i < n; ++i) {
+        sum += v[i];
+        cnt += pre[sum - k];
+        ++pre[sum];
+    }
+    return cnt;
 }
