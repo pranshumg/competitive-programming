9fdd2531c085aacc111f944fb7bd733213054db0
Pranshu Gupta <pranshumg@gmail.com>
2026-01-25 13:05:16 +0530

    unequal no. of pos and neg elements

--- arrays/rearrange_elements_by_sign/unequal.cpp
+++ arrays/rearrange_elements_by_sign/unequal.cpp
@@ -3,35 +3,44 @@
 using namespace std;
 
 /* Rearrange unequal positive and negative elements by sign */
+
 // TC - O(n), SC - O(n)
 vector<int> rearrange_array(vector<int>& v, int n) {
-  vector<int> pos, neg;
-  for (int i = 0; i < n; i++) {
-    if (v[i] > 0) {
-      pos.push_back(v[i]);
-    } else {
-      neg.push_back(v[i]);
+    vector<int> pos, neg;
+    // Separate positive and negative elements
+    for (int i = 0; i < n; ++i) {
+        if (v[i] > 0) {
+            pos.emplace_back(v[i]);
+        } else {
+            neg.emplace_back(v[i]);
+        }
     }
-  }
-  vector<int> res(n);
-  if ((int)pos.size() > (int)neg.size()) {
-    for (int i = 0; i < (int)neg.size(); i++) {
-      res[2 * i] = pos[i];
-      res[2 * i + 1] = neg[i];
+    vector<int> res(n);
+    // If positives are more than negatives
+    if ((int)pos.size() > (int)neg.size()) {
+        // Fill alternating positions up to the size of the smaller list (neg)
+        for (int i = 0; i < (int)neg.size(); ++i) {
+            res[2 * i] = pos[i];
+            res[2 * i + 1] = neg[i];
+        }
+        // Append remaining positives
+        int idx = (int)neg.size() * 2;
+        for (int i = (int)neg.size(); i < (int)pos.size(); ++i) {
+            res[idx++] = pos[i];
+        }
+    } 
+    // If negatives are more than positives (or equal, though equal is usually handled by the simpler logic)
+    else {
+        // Fill alternating positions up to the size of the smaller list (pos)
+        for (int i = 0; i < (int)pos.size(); ++i) {
+            res[2 * i] = pos[i];
+            res[2 * i + 1] = neg[i];
+        }
+        // Append remaining negatives
+        int idx = (int)pos.size() * 2;
+        for (int i = (int)pos.size(); i < (int)neg.size(); ++i) {
+            res[idx++] = neg[i];
+        }
     }
-    int idx = (int)neg.size() * 2;
-    for (int i = (int)neg.size(); i < (int)pos.size(); i++) {
-      res[idx++] = pos[i];
-    }
-  } else {
-    for (int i = 0; i < (int)pos.size(); i++) {
-      res[2 * i] = pos[i];
-      res[2 * i + 1] = neg[i];
-    }
-    int idx = (int)pos.size() * 2;
-    for (int i = (int)pos.size(); i < (int)neg.size(); i++) {
-      res[idx++] = neg[i];
-    }
-  }
-  return res;
+    return res;
 }
