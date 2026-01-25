f16991762eb9b6a3d252e92e5c20872f6d15f5a7
Pranshu Gupta <pranshumg@gmail.com>
2026-01-25 13:09:37 +0530

    best time to buy and sell stock

--- arrays/buy_and_sell_stock.cpp
+++ arrays/buy_and_sell_stock.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 /* Best time to buy and sell stock */
+
 // TC - O(n), SC - O(1)
 int max_profit(vector<int>& v, int n) {
-  int mini = v[0], profit = 0;
-  for (int i = 0; i < n; i++) {
-    profit = max(profit, v[i] - mini);
-    mini = min(mini, v[i]);
-  }
-  return profit;
+    int mn = v[0], profit = 0;
+    for (int i = 0; i < n; ++i) {
+        // Calculate the profit if we sell at current price v[i]
+        // using the minimum price found so far
+        int cost = v[i] - mn;
+        // Update max profit if current cost is higher
+        profit = max(profit, cost);
+        // Update minimum price seen so far
+        mn = min(mn, v[i]);
+    }
+    return profit;
 }
