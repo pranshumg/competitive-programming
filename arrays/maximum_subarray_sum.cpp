d1aa0ff05e6b49cd3716a98d094c8c95dd758d87
Pranshu Gupta <pranshumg@gmail.com>
2026-01-25 12:50:25 +0530

    maximum subarray sum

--- arrays/maximum_subarray_sum.cpp
+++ arrays/maximum_subarray_sum.cpp
@@ -3,48 +3,59 @@
 using namespace std;
 
 /* Maximum subarray sum */
-// Brute (TC - O(n³), SC - O(1))
+
+// Brute 
+// TC - O(n * n * n), SC - O(1)
 int max_subarray_sum(vector<int>& v, int n) {
-  int ans = INT_MIN;
-  for (int i = 0; i < n; i++) {
-    for (int j = i; j < n; j++) {
-      int sum = 0;
-      for (int k = i; k <= j; k++) {
-        sum += v[k];
-      }
-      ans = max(ans, sum);
+    int ans = INT_MIN;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i; j < n; ++j) {
+            int sum = 0;
+            for (int k = i; k <= j; ++k) {
+                sum += v[k];
+            }
+            ans = max(ans, sum);
+        }
     }
-  }
-  return ans;
+    return ans;
 }
 
-// Better (TC - O(n²), SC - O(1))
+// Better Approach
+// TC - O(n * n), SC - O(1)
 int max_subarray_sum(vector<int>& v, int n) {
-  int ans = INT_MIN;
-  for (int i = 0; i < n; i++) {
-    int sum = 0;
-    for (int j = i; j < n; j++) {
-      sum += v[j];
-      ans = max(ans, sum);
+    int ans = INT_MIN;
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += v[j];
+            ans = max(ans, sum);
+        }
     }
-  }
-  return ans;
+    return ans;
 }
 
 // Kadane's Algorithm (Optimal)
 // TC - O(n), SC - O(1)
 int max_subarray_sum(vector<int>& v, int n) {
-  int ans = v[0], sum = v[0], low = 0, ans_low = -1, ans_high = -1;
-  for (int i = 1; i < n; i++) {
-    if (sum + v[i] < v[i]) {
-      sum = v[i], low = i;
-    } else {
-      sum += v[i];
+    // Handling the case where n is 0 if necessary, though problem constraints usually imply n >= 1
+    if (n == 0) {
+        return 0;
     }
-    if (sum > ans) {
-      ans = sum;
-      ans_low = low, ans_high = i;
-    }
-  }
-  return ans;
+    int ans = v[0], sum = v[0], l = 0;
+    // Variables to store the actual subarray indices if needed
+    int ans_l = 0, ans_h = 0; 
+    for (int i = 1; i < n; ++i) {
+        // If adding the current number to previous sum is worse than 
+        // starting fresh at current number (i.e., previous sum was negative)
+        if (sum + v[i] < v[i]) {
+            sum = v[i], l = i;
+        } else {
+            sum += v[i];
+        }
+        if (sum > ans) {
+            ans = sum, ans_l = l, ans_h = i;
+        }
+    }  
+    // If you need to print the subarray, you can use v[ans_low]...v[ans_high]
+    return ans;
 }
