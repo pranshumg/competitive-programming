f90d20077e0c68b8392d58ff0d580a61f80d38bf
Pranshu Gupta <pranshumg@gmail.com>
2026-01-25 12:58:23 +0530

    equal no. of pos and neg elements

--- arrays/rearrange_elements_by_sign/equal.cpp
+++ arrays/rearrange_elements_by_sign/equal.cpp
@@ -3,34 +3,38 @@
 using namespace std;
 
 /* Rearrange equal positive and negative elements by sign */
-// Brute (TC - O(n), SC - O(n))
+
+// Brute 
+// TC - O(n), SC - O(n)
 vector<int> rearrange_array(vector<int>& v, int n) {
-  vector<int> pos, neg;
-  for (int i = 0; i < n; i++) {
-    if (v[i] > 0) {
-      pos.push_back(v[i]);
-    } else {
-      neg.push_back(v[i]);
-    }
-  }
-  vector<int> res(n);
-  for (int i = 0; i < n / 2; i++) {
-    res[2 * i] = pos[i];
-    res[2 * i + 1] = neg[i];
-  }
-  return res;
+    vector<int> pos, neg;
+    for (int i = 0; i < n; ++i) {
+        if (v[i] > 0) {
+            pos.emplace_back(v[i]);
+        } else {
+            neg.emplace_back(v[i]);
+        }
+    }    
+    vector<int> res(n);
+    for (int i = 0; i < n / 2; ++i) {
+        res[2 * i] = pos[i];
+        res[2 * i + 1] = neg[i];
+    }   
+    return res;
 }
 
-// Optimal (TC - O(n), SC - O(n))
+// Optimal Approach (Two Pointers)
+// TC - O(n), SC - O(n)
+// Note: We still use O(n) space for the result, but we avoid the extra pass and vectors.
 vector<int> rearrange_array(vector<int>& v, int n) {
-  vector<int> res(n);
-  int pos = 0, neg = 1;
-  for (int i = 0; i < n; i++) {
-    if (v[i] > 0) {
-      res[pos] = v[i], pos += 2;
-    } else {
-      res[neg] = v[i], neg += 2;
+    vector<int> res(n);
+    int pos = 0, neg = 1; 
+    for (int i = 0; i < n; ++i) {
+        if (v[i] > 0) {
+            res[pos] = v[i], pos += 2;
+        } else {
+            res[neg] = v[i], neg += 2;
+        }
     }
-  }
-  return res;
+    return res;
 }
