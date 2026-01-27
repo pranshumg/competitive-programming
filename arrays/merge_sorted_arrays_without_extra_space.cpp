da97ee8feb6753fa27f2a32bf2ebfd0c17e07d94
Pranshu Gupta <pranshumg@gmail.com>
2026-01-27 22:36:39 +0530

    merge two sorted arrays without extra space

--- arrays/merge_sorted_arrays_without_extra_space.cpp
+++ arrays/merge_sorted_arrays_without_extra_space.cpp
@@ -3,44 +3,56 @@
 using namespace std;
 
 /* Merge sorted arrays without extra space */
-// Optimal 1 (TC - O(m log m + n log n), SC - O(1))
+
+// Optimal 1 
+// TC - O(min(m, n)) + O(m log m + n log n), SC - O(1)
+// This approach swaps elements and then re-sorts both arrays.
 void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-  int left = m - 1, right = 0;
-  while (left >= 0 && right < n) {
-    if (v1[left] > v2[right]) {
-      swap(v1[left--], v2[right++]);
-    } else {
-      break;
+    int l = m - 1, r = 0;
+    while (l >= 0 && r < n) {
+        if (v1[l] > v2[r]) {
+            swap(v1[l--], v2[r++]);
+        } else {
+            break;
+        }
     }
-  }
-  sort(v1.begin(), v1.end());
-  sort(v2.begin(), v2.end());
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
 }
 
-// Optimal 2 (gap method) (TC - O((m + n) log (m + n)), SC - O(1))
-void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-  int len = n + m, gap = (len + 1) / 2;
-  while (gap > 0) {
-    int left = 0, right = left + gap;
-    while (right < len) {
-      if (right < m) {
-        if (v1[left] > v1[right]) {
-          swap(v1[left], v1[right]);
+// Optimal 2 (Gap Method) 
+// TC - O((m + n) log (m + n)), SC - O(1)
+// This is based on Shell Sort logic.
+void merge_gap(vector<int>& v1, int m, vector<int>& v2, int n) {
+    int len = n + m;
+    int gap = (len / 2) + (len % 2); // Same as ceil(len / 2)
+    while (gap > 0) {
+        int l = 0;
+        int r = l + gap;
+        while (r < len) {
+            // Case 1: Both pointers in the first array (v1)
+            if (r < m) {
+                if (v1[l] > v1[r]) {
+                    swap(v1[l], v1[r]);
+                }
+            }
+            // Case 2: Left pointer in v1, Right pointer in v2
+            else if (l < m && r >= m) {
+                if (v1[l] > v2[r - m]) {
+                    swap(v1[l], v2[r - m]);
+                }
+            }
+            // Case 3: Both pointers in the second array (v2)
+            else {
+                if (v2[l - m] > v2[r - m]) {
+                    swap(v2[l - m], v2[r - m]);
+                }
+            }
+            ++l, ++r;
         }
-      } else if (left < m && right >= m) {
-        if (v1[left] > v2[right - m]) {
-          swap(v1[left], v2[right - m]);
+        if (gap == 1) {
+            break;
         }
-      } else {
-        if (v2[left - m] > v2[right - m]) {
-          swap(v2[left - m], v2[right - m]);
-        }
-      }
-      left++, right++;
+        gap = (gap / 2) + (gap % 2);
     }
-    if (gap == 1) {
-      break;
-    }
-    gap = (gap + 1) / 2;
-  }
 }
