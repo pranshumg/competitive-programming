4a62c7079a86363f0ee509aeca9cae8d8f1db44d
Pranshu Gupta <pranshumg@gmail.com>
2026-01-27 22:57:25 +0530

    linear search

--- arrays/linear_search.cpp
+++ arrays/linear_search.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 /* Linear search */
+
 // TC - O(n), SC - O(1)
 int linear_search(vector<int>& v, int n, int k) {
-  for (int i = 0; i < n; i++) {
-    if (v[i] == k) {
-      return i;
+    for (int i = 0; i < n; ++i) {
+        if (v[i] == k) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
