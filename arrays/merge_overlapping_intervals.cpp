f212187125896f3990751dd7bbf250ec9a7d157d
Pranshu Gupta <pranshumg@gmail.com>
2026-01-27 22:10:49 +0530

    merge overlapping intervals

--- arrays/merge_overlapping_intervals.cpp
+++ arrays/merge_overlapping_intervals.cpp
@@ -3,37 +3,44 @@
 using namespace std;
 
 /* Merge overlapping intervals */
-// Brute (TC - O(n log n), SC - O(n))
+
+// Brute 
+// TC - O(n log n), SC - O(n)
 vector<vector<int>> merge(vector<vector<int>>& v, int n) {
-  vector<vector<int>> ans;
-  sort(v.begin(), v.end());
-  for (int i = 0; i < n; i++) {
-    int first = v[i][0], second = v[i][1];
-    if (!ans.empty() && second <= ans.back()[1]) {
-      continue;
+    vector<vector<int>> ans;
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; ++i) {
+        int f = v[i][0], s = v[i][1];
+        // Skip if this interval is already merged into the last one in ans
+        if (!ans.empty() && s <= ans.back()[1]) {
+            continue;
+        }
+        for (int j = i + 1; j < n; ++j) {
+            if (v[j][0] <= s) {
+                s = max(s, v[j][1]);
+            } else {
+                break;
+            }
+        }
+        ans.emplace_back(f, s);
     }
-    for (int j = i + 1; j < n; j++) {
-      if (v[j][0] <= second) {
-        second = max(second, v[j][1]);
-      } else {
-        break;
-      }
-    }
-    ans.push_back({first, second});
-  }
-  return ans;
+    return ans;
 }
 
-// Optimal (TC - O(n log n), SC - O(n))
+// Optimal 
+// TC - O(n log n), SC - O(n)
 vector<vector<int>> merge(vector<vector<int>>& v, int n) {
-  vector<vector<int>> ans;
-  sort(v.begin(), v.end());
-  for (int i = 0; i < n; i++) {
-    if (ans.empty() || v[i][0] > ans.back()[1]) {
-      ans.push_back(v[i]);
-    } else {
-      ans.back()[1] = max(ans.back()[1], v[i][1]);
+    vector<vector<int>> ans;
+    sort(v.begin(), v.end());
+    for (int i = 0; i < n; ++i) {
+        // If ans is empty or current interval does not overlap with the last one
+        if (ans.empty() || v[i][0] > ans.back()[1]) {
+            ans.emplace_back(v[i]);
+        } 
+        // Overlap detected:  merge by updating the end time
+        else {
+            ans.back()[1] = max(ans.back()[1], v[i][1]);
+        }
     }
-  }
-  return ans;
+    return ans;
 }
