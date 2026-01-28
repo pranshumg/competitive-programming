a831e3d21e92bb50c8e8b112c69387ad55516940
Pranshu Gupta <pranshumg@gmail.com>
2026-01-28 10:54:49 +0530

    added string method

--- maths/count_digits.cpp
+++ maths/count_digits.cpp
@@ -19,6 +19,12 @@ int count_digits(int n) {
 
 // TC - O(1)
 int count_digits(int n) {
+    string s = to_string(n);
+    return n < 0 ? (int)s.size() - 1 : (int)s.size();
+}
+
+// TC - O(1)
+int count_digits(int n) {
     n = abs(n);
     return n ? (int)(log10(n) + 1) : 1;
 }
