246bcd5073e570a7b4b631ec14740f262904f1f1
Pranshu Gupta <pranshumg@gmail.com>
2026-01-29 15:22:31 +0530

    jump game

--- greedy/jump_game.cpp
+++ greedy/jump_game.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 // TC - O(n), SC - O(1)
 bool can_jump(vector<int>& v) {
-  int n = (int)v.size();
-  int max_idx = 0;
-  for (int i = 0; i < n; ++i) {
-    if (max_idx < i) {
-      return false;
+    int mx_idx = 0;
+    for (int i = 0; i < (int)v.size(); ++i) {
+        // If the current index i is beyond the maximum reach calculated so far,
+        // we cannot reach this point (and consequently the end).
+        if (mx_idx < i) {
+            return false;
+        }
+        // Update the maximum index reachable from the current position
+        mx_idx = max(mx_idx, i + v[i]);
     }
-    max_idx = max(max_idx, i + v[i]);
-  }
-  return true;
+    return true;
 }
