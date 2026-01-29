c1c2a37e0464bf60878e7d3d710de30130dac486
Pranshu Gupta <pranshumg@gmail.com>
2026-01-29 14:54:43 +0530

    assign cookies

--- greedy/assign_cookies.cpp
+++ greedy/assign_cookies.cpp
@@ -5,16 +5,15 @@ using namespace std;
 /* Assign cookies */
 
 // TC - O(n log n + m log m), SC - O(1)
-int assign_cookies(vector<int> &greed, vector<int> &size) {
-  int n = (int)greed.size(), m = (int)size.size();
-  sort(greed.begin(), greed.end());
-  sort(size.begin(), size.end());
-  int left = 0, right = 0;
-  while (left < m && right < n) {
-    if (greed[right] <= size[left]) {
-      ++right;
+int assign_cookies(vector<int> &size, vector<int> &greed) {
+    sort(size.begin(), size.end());
+    sort(greed.begin(), greed.end());
+    int l = 0, r = 0;
+    while (l < (int)size.size() && r < (int)greed.size()) {
+        if (size[l] >= greed[r]) {
+            ++r;
+        }
+        ++l;
     }
-    ++left;
-  }
-  return right;
+    return r;
 }
