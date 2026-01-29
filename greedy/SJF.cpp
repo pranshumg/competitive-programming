c4e1f906a1e17b672692b745d2d8cd3c28b7eb22
Pranshu Gupta <pranshumg@gmail.com>
2026-01-29 15:13:17 +0530

    Shortest Job first CPU Scheduling

--- greedy/SJF.cpp
+++ greedy/SJF.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-/* Shorted Job First CPU scheduling */
+/* Shortest Job First CPU scheduling */
 
 // TC - O(n log n), SC - O(1)
+// Note: Returns integer average (truncated). Change return type to float/double for precision.
 int average(vector<int>& v) {
-  int n = (int)v.size();  
-  sort(v.begin(), v.end());
-  int t = 0, wt = 0;
-  for (auto &it : v) {
-    wt += t;
-    t += it;
-  }
-  return wt / n;
+    sort(v.begin(), v.end());
+    int t = 0, wt = 0;
+    for (auto &it : v) {
+        wt += t;     // Waiting time for current process is the total time elapsed so far
+        t += it;     // Update total time by adding current process burst time
+    }
+    return wt / (int)v.size();
 }
