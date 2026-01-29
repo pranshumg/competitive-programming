792ae57c58aa200f4964044c81ead2d98c8b05fb
Pranshu Gupta <pranshumg@gmail.com>
2026-01-29 15:01:41 +0530

    lemonade change

--- greedy/lemonade.cpp
+++ greedy/lemonade.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 // TC - O(n), SC - O(1)
 bool lemonade(vector<int>& v) {
-  int five = 0, ten = 0;
-  for (int i = 0; i < (int)v.size(); ++i) {
-    if (v[i] == 5) {
-      ++five;
-    } else if (v[i] == 10) {
-      if (five) {
-        --five, ++ten;
-      } else {
-        return false;
-      }
-    } else {
-      if (five && ten) {
-        --five, --ten;
-      } else if (five >= 3) {
-        five -= 3;
-      } else {
-        return false;
-      }
+    int five = 0, ten = 0;
+    for (int i = 0; i < (int)v.size(); ++i) {
+        if (v[i] == 5) {
+            ++five;
+        } else if (v[i] == 10) {
+            if (five) {
+                --five, ++ten;
+            } else {
+                return false;
+            }
+        } else {
+            // Customer gives $20
+            // Greedy Strategy: Always prefer giving back $10 + $5 over $5 + $5 + $5.
+            // Why? Because $5 bills are more useful (required for both $10 and $20 transactions).
+            if (five && ten) {
+                --five, --ten;
+            } else if (five > 2) {
+                five -= 3;
+            } else {
+                return false;
+            }
+        }
     }
-  }
-  return true;
+    return true;
 }
