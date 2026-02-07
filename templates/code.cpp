43b832155198dc36e60f8f9d4fb789f57926956b
Pranshu Gupta <pranshumg@gmail.com>
2026-02-07 11:45:37 +0530

    removed macro

--- templates/code.cpp
+++ templates/code.cpp
@@ -8,14 +8,12 @@ using u32 = uint32_t;
 using i128 = __int128_t;
 using u128 = __uint128_t;
 
-#define all(x) (x).begin(), (x).end()
-
 void solve() {
     $0
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin >> t;
