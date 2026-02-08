b3c89774c068c1dc78ef869bc38ce649ac49488c
Pranshu Gupta <pranshumg@gmail.com>
2026-02-08 21:58:14 +0530

    find element with given row and column

--- arrays/pascal_triangle/find_element.cpp
+++ arrays/pascal_triangle/find_element.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 /* find the element at given row and column */
+
 // TC - O(r), SC - O(1)
 int64_t nCr(int n, int r) {
-  int64_t res = 1;
-  for (int i = 0; i < r; i++) {
-    res = res * (n - i);
-    res = res / (i + 1);
-  }
-  return res;
+    int64_t res = 1;
+    for (int i = 0; i < r; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
 }
