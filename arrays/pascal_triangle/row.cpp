5a74c75c6fde5659234454095f5af24dafc0ae2d
Pranshu Gupta <pranshumg@gmail.com>
2026-02-08 21:58:48 +0530

    print any nth row of pascal triangle

--- arrays/pascal_triangle/row.cpp
+++ arrays/pascal_triangle/row.cpp
@@ -3,29 +3,34 @@
 using namespace std;
 
 /* Print any nth row of Pascal Triangle */
-// Brute (TC - O(r²), SC - O(1))
+
 int64_t nCr(int n, int r) {
-  int64_t res = 1;
-  for (int i = 0; i < r; i++) {
-    res = res * (n - i);
-    res = res / (i + 1);
-  }
-  return res;
+    int64_t res = 1;
+    for (int i = 0; i < r; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
 }
 
+// Brute Force approach using nCr function
+// TC - O(r * r), SC - O(1)
 void print_row(int r) {
-  for (int c = 1; c <= r; c++) {
-    cout << nCr(r - 1, c - 1) << ' ';
-  }
+    for (int c = 1; c <= r; ++c) {
+        cout << nCr(r - 1, c - 1) << ' ';
+    }
+    cout << '\n';
 }
 
-// Optimal (TC - O(r), SC - O(1))
+// Optimal approach using the property: Next Term = Current Term * (row - col) / col
+// TC - O(r), SC - O(1)
 void print_row(int r) {
-  int ans = 1;
-  cout << ans << ' ';
-  for (int i = 1; i < r; i++) {
-    ans = ans * (r - i);
-    ans = ans / i;
-    cout << ans << ' ';
-  }
+    int64_t ans = 1;
+    cout << ans << ' '; // The first element is always 1
+    for (int i = 1; i < r; ++i) {
+        ans *= (r - i);
+        ans /= i;
+        cout << ans << ' ';
+    }
+    cout << '\n';
 }
