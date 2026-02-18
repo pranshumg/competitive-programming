c4d54a4c5daff0062a6b3c3e98b3c8f2540a0691
Pranshu Gupta <pranshumg@gmail.com>
2026-02-18 21:49:45 +0530

    intro to binary search

--- binary_search/introduction.cpp
+++ binary_search/introduction.cpp
@@ -4,33 +4,36 @@ using namespace std;
 
 /* Binary search */
 
-// Iterative (TC - O(log n), SC - O(1))
+// Iterative 
+// TC - O(log n), SC - O(1)
 int binary_search(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (v[mid] == target) {
-      return mid;
-    } else if (v[mid] > target) {
-      high = mid - 1;
-    } else {
-      low = mid + 1;
+    int l = 0, h = n - 1;
+    while (l <= h) {
+        // Use this to prevent potential overflow instead of (l + h) >> 1
+        int m = l + ((h - l) >> 1);
+        if (v[m] == target) {
+            return m;
+        } else if (v[m] > target) {
+            h = m - 1;
+        } else {
+            l = m + 1;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
-// Recursive (TC - O(log n), SC - O(log n))
-int binary_search(vector<int>& v, int target, int low, int high) {
-  if (low > high) {
-    return -1;
-  }
-  int mid = (low + high) / 2;
-  if (v[mid] == target) {
-    return mid;
-  } else if (v[mid] > target) {
-    return binary_search(v, target, low, mid - 1);
-  } else {
-    return binary_search(v, target, mid + 1, high);
-  }
+// Recursive 
+// TC - O(log n), SC - O(log n) due to recursion stack
+int binary_search(vector<int>& v, int target, int l, int h) {
+    if (l > h) {
+        return -1;
+    }
+    int m = l + ((h - l) >> 1);
+    if (v[m] == target) {
+        return m;
+    } else if (v[m] > target) {
+        return binary_search(v, target, l, m - 1);
+    } else {
+        return binary_search(v, target, m + 1, h);
+    }
 }
