37ff495903c24b6065a4860f01f958ca1e218369
Pranshu Gupta <pranshumg@gmail.com>
2026-02-18 21:57:56 +0530

    upper bound

--- binary_search/upper_bound.cpp
+++ binary_search/upper_bound.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-/* upper bound */
-// TC - O(log n), SC - O(1)
+/* Upper Bound: Finds the first index `i` such that v[i] > target.
+   TC - O(log n), SC - O(1)
+*/
 int upper_bound(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1, ans = n;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (v[mid] > target) {
-      ans = mid, high = mid - 1;
-    } else {
-      low = mid + 1;
+    int l = 0, h = n - 1;
+    while (l <= h) {
+        int m = l + ((h - l) >> 1);
+        if (v[m] > target) {
+            // This is a candidate for Upper Bound, but check 
+            // if there's an even smaller index to the left.
+            h = m - 1;
+        } else {
+            // If v[mid] <= target, the upper bound must be further right.
+            l = m + 1;
+        }
     }
-  }
-  return ans;
+    // 'l' will naturally settle at the first index where v[i] > target.
+    return l;
 }
