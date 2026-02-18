ee906f4274aee1c63fb1d0736a07cc23a2058414
Pranshu Gupta <pranshumg@gmail.com>
2026-02-18 22:12:13 +0530

    floor

--- binary_search/floor.cpp
+++ binary_search/floor.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
-/* Floor */
-// TC - O(log n), SC - O(1)
+/* Floor: Finds the largest index `i` such that v[i] <= target.
+   TC - O(log n), SC - O(1)
+*/
 int floor(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1, ans = -1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (v[mid] <= target) {
-      ans = mid, low = mid + 1;
-    } else {
-      high = mid - 1;
+    int l = 0, h = n - 1;
+    while (l <= h) {
+        int m = l + ((h - l) >> 1);
+        if (v[m] <= target) {
+            // v[m] is a valid candidate for floor, 
+            // so we look to the right to see if a larger valid index exists.
+            l = m + 1;
+        } else {
+            // v[m] is too large, move the search left.
+            h = m - 1;
+        }
     }
-  }
-  return ans;
+    // After the loop, 'h' will be the largest index such that v[h] <= target.
+    // If no such element exists, 'h' will naturally become -1.
+    return h;
 }
