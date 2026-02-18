e630fa86d918a989a0639075a15bf932ef2fc221
Pranshu Gupta <pranshumg@gmail.com>
2026-02-18 22:14:39 +0530

    ceil

--- binary_search/ceil.cpp
+++ binary_search/ceil.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-/* Ceil */
-// TC - O(log n), SC - O(1)
+/* Ceil: Finds the smallest index `i` such that v[i] >= target.
+   TC - O(log n), SC - O(1)
+*/
 int ceil(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1, ans = -1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (v[mid] >= target) {
-      ans = mid, high = mid - 1;
-    } else {
-      low = mid + 1;
-    }
-  }
-  return ans;
+    int l = 0, h = n - 1;
+    while (l <= h) {
+        int m = l + ((h - l) >> 1);   
+        if (v[m] >= target) {
+            // Potential ceiling found, but check for a smaller index to the left
+            h = m - 1;
+        } else {
+            // v[m] is strictly less than target, ceiling must be to the right
+            l = m + 1;
+        }
+    }   
+    // 'l' will point to the first index where v[i] >= target.
+    // Note: If target > v[n-1], 'l' will be equal to 'n'.
+    return l;
 }
