bdba67f2bdff615af1df0db4b8d3bb2d074e462c
Pranshu Gupta <pranshumg@gmail.com>
2026-02-22 16:31:10 +0530

    corrected runtime error

--- sorting/insertion_sort.cpp
+++ sorting/insertion_sort.cpp
@@ -9,7 +9,8 @@ void insertion_sort(vector<int>& v, int n) {
     for (int i = 1; i < n; ++i) {
         int j = i;
         while (j > 0 && v[j - 1] > v[j]) {
-            swap(v[j - 1], v[j--]);
+            swap(v[j - 1], v[j]);
+            --j;
         }
     }
 }
