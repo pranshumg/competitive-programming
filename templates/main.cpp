b82bc35690f8538901329b269ec341512ff2ec4d
Pranshu Gupta <pranshumg@gmail.com>
2026-03-04 04:38:52 +0530

    updated

--- templates/main.cpp
+++ templates/main.cpp
@@ -2,23 +2,9 @@
 
 using namespace std;
 
-using i64 = int64_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-
-void solve() {
-    $0
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    $0
     return 0;
 }
