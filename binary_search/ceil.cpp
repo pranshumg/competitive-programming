9fef9c1fb3e9343d54e530a628b91d0942395664
Pranshu Gupta <pranshumg@gmail.com>
2026-03-06 22:12:00 +0530

    updated tab space

--- binary_search/ceil.cpp
+++ binary_search/ceil.cpp
@@ -6,18 +6,18 @@ using namespace std;
    TC - O(log n), SC - O(1)
 */
 int ceil(vector<int>& v, int n, int target) {
-    int l = 0, h = n - 1;
-    while (l <= h) {
-        int m = l + ((h - l) >> 1);   
-        if (v[m] >= target) {
-            // Potential ceiling found, but check for a smaller index to the left
-            h = m - 1;
-        } else {
-            // v[m] is strictly less than target, ceiling must be to the right
-            l = m + 1;
-        }
-    }   
-    // 'l' will point to the first index where v[i] >= target.
-    // Note: If target > v[n-1], 'l' will be equal to 'n'.
-    return l;
+  int low = 0, high = n - 1;
+  while (low <= high) {
+    int mid = low + (high - low) / 2;   
+    if (v[mid] >= target) {
+      // Potential ceiling found, but check for a smaller index to the left
+      high = mid - 1;
+    } else {
+      // v[mid] is strictly less than target, ceiling must be to the right
+      low = mid + 1;
+    }
+  }   
+  // 'low' will point to the first index where v[i] >= target.
+  // Note: If target > v[n-1], 'low' will be equal to 'n'.
+  return low;
 }
