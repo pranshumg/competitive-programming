d4c3d0c8bd4eb9e5fb4f1fbd1820104221df6091
Pranshu Gupta <pranshumg@gmail.com>
2026-03-06 23:15:00 +0530

    updated code

--- binary_search/count_occurrences.cpp
+++ binary_search/count_occurrences.cpp
@@ -3,49 +3,46 @@
 using namespace std;
 
 /* Count occurrences */
-// TC - O(log n), SC - O(1)
-int first(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1, first = -1;
+
+int first(vector<int>&v, int n, int target) {
+  int low = 0, high = n - 1;
+  int idx = -1;
   while (low <= high) {
-    int mid = (low + high) / 2;
+    int mid = low + (high - low) / 2;
     if (v[mid] == target) {
-      first = mid, high = mid - 1;
-    } else if (v[mid] < target) {
-      low = mid + 1;
-    } else {
+      idx = mid;
       high = mid - 1;
+    } else if (v[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
   }
-  return first;
+  return idx;
 }
 
 int last(vector<int>& v, int n, int target) {
-  int low = 0, high = n - 1, last = -1;
+  int low = 0, high = n - 1;
+  int idx = -1;
   while (low <= high) {
-    int mid = (low + high) / 2;
+    int mid = low + (high - low) / 2;
     if (v[mid] == target) {
-      last = mid, low = mid + 1;
+      idx = mid;
+      low = mid + 1;
     } else if (v[mid] < target) {
       low = mid + 1;
     } else {
       high = mid - 1;
     }
   }
-  return last;
+  return idx;
 }
 
-pair<int, int> search(vector<int>& v, int target) {
-  int f = first(v, (int)v.size(), target);
+// TC - O(log n), SC - O(n)
+int count(vector<int>& v, int n, int target) {
+  int f = first(v, n, target);
   if (f == -1) {
-    return {-1, -1};
-  } 
-  return {f, last(v, (int)v.size(), target)};
-}
-
-int count(vector<int>& v, int target) {
-  pair<int, int> p = search(v, target);
-  if (p.first == -1) {
     return 0;
   }
-  return p.second - p.first + 1;
+  return last(v, n, target) - f + 1;
 }
