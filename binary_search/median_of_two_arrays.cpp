b3cc2cac71a45d3f1e9f260adcedf0a65081e420
Pranshu Gupta <pranshumg@gmail.com>
2026-03-18 02:31:28 +0530

    updated code and added comments

--- binary_search/median_of_two_arrays.cpp
+++ binary_search/median_of_two_arrays.cpp
@@ -4,119 +4,65 @@ using namespace std;
 
 /* Median of two sorted arrays */
 
-// Brute
-// TC - O(n + m), SC - O(n + m)
-double median(vector<int>& v, int n, vector<int>& w, int m) {
-  vector<int> merged;
+// APPROACH 1: Merge and Store
+// TC -  O(n + m), SC -  O(n + m) 
+double median(vector<int>& v1, int m, vector<int>& v2, int n) {
+  vector<int> res;
   int i = 0, j = 0;
-  while (i < n && j < m) {
-    if (v[i] <= w[j]) {
-      merged.emplace_back(v[i++]);
+  // Standard merge process to combine two sorted arrays
+  while (i < m && j < n) {
+    if (v1[i] < v2[j]) {
+      res.emplace_back(v1[i++]);
     } else {
-      merged.emplace_back(w[j++]);
+      res.emplace_back(v2[j++]);
     }
   }
-  while (i < n) {
-    merged.emplace_back(v[i++]);
-  }
-  while (j < m) {
-    merged.emplace_back(w[j++]);
-  }
-  int size = n + m;
-  if (size % 2 == 1) {
-    return merged[size / 2];
-  }
-  return ((double)merged[size / 2] + (double)merged[size / 2 - 1]) / 2.0;
+  // Add remaining elements from v1 if any
+  while (i < n) res.emplace_back(v1[i++]);
+  // Add remaining elements from v2 if any
+  while (j < m) res.emplace_back(v2[j++]);
+  // If total elements are odd, return the middle element
+  if ((n + m) & 1) return res[(n + m) / 2];
+  // If even, return average of the two middle elements
+  return (res[(n + m) / 2] + res[(n + m) / 2 + 1]) / double(2);
 }
 
-// Better
-// TC - O(n + m), SC - O(1)
-double median(vector<int>& v, int n, vector<int>& w, int m) {
-  int size = n + m;
-  int idx1 = size / 2 - 1, idx2 = n / 2;
-  int el1 = -1, el2 = -1;
+// APPROACH 2: Optimized Space (Pointer Tracking)
+// TC - O(n + m), SC - O(1) 
+double median(vector<int>& v1, int n, vector<int>& v2, int m) {
   int cnt = 0;
+  // Target indices for the median elements
+  int idx1 = (n + m) / 2, idx2 = (n + m) / 2 + 1;
+  int el1 = -1, el2 = -1;
   int i = 0, j = 0;
+  // Iterate until we find the median positions without storing elements
   while (i < n && j < m) {
-    if (v[i] <= w[j]) {
-      if (cnt == idx1) {
-        el1 = v[i];
-      }
-      if (cnt == idx2) {
-        el2 = v[i]; 
-      }
-      cnt++, i++;
+    if (v1[i] < v2[j]) {
+      if (cnt == idx1) el1 = v1[i];
+      if (cnt == idx2) el2 = v1[i];
+      ++i;
     } else {
-      if (cnt == idx1) {
-        el1 = w[j];
-      }
-      if (cnt == idx2) {
-        el2 = w[j]; 
-      }
-      cnt++, j++;
+      if (cnt == idx1) el1 = v2[j];
+      if (cnt == idx2) el2 = v2[j];
+      ++j;
     }
-  } 
+    ++cnt;
+  }
+  // Handle remaining elements in v1
   while (i < n) {
-    if (cnt == idx1) {
-      el1 = v[i];
-    }
-    if (cnt == idx2) {
-      el2 = v[i]; 
-    }
-    cnt++, i++;
+    if (cnt == idx1) el1 = v1[i];
+    if (cnt == idx2) el2 = v1[i];
+    ++i;
+    ++cnt;
   }
+  // Handle remaining elements in v2
   while (j < m) {
-    if (cnt == idx1) {
-      el1 = w[j];
-    }
-    if (cnt == idx2) {
-      el2 = w[j]; 
-    }
-    cnt++, j++;
+    if (cnt == idx1) el1 = v2[j];
+    if (cnt == idx2) el2 = v2[j];
+    ++j;
+    ++cnt;
   }
-  if (size % 2 == 1) {
-    return el2;
-  }
-  return (double)((double)(el1 + el2)) / 2.0;
+  // Check parity of total size to return the correct median
+  if ((n + m) & 1) return el1;
+  return (el1 + el2) / double(2);
 }
-
-// Binary Search
-// TC - O(log(min(n, m))), SC - O(1)
-double median(vector<int>& v, int n, vector<int>& w, int m) {
-  if (n > m) {
-    return median(w, m, v, n);
-  }
-  int low = 0, high = n;
-  int left = (n + m + 1) / 2;
-  int size = n + m;
-  while (low <= high) {
-    int mid1 = (low + high) / 2;
-    int mid2 = left - mid1;
-    int l1 = INT_MIN, l2 = INT_MIN;
-    int r1 = INT_MAX, r2 = INT_MAX;
-    if (mid1 < n) {
-      r1 = v[mid1];
-    }
-    if (mid2 < m) {
-      r2 = w[mid2];
-    }
-    if (mid1 > 0) {
-      l1 = v[mid1 - 1];
-    }
-    if (mid2 > 0) {
-      l2 = w[mid2 - 1];
-    }
-    if (l1 <= r2 && l2 <= r1) {
-      if (size % 2 == 1) {
-        return max(l1, l2);
-      }
-      return ((double)(max(l1, l2) + min(r1, r2)) / 2.0);
-    } else if (l1 > r2) {
-      high = mid1 -1;
-    } else {
-      low = mid1 + 1;
-    }
-  }
-  return 0;
-}
-  
