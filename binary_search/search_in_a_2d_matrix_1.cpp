0dfc95a1d7aa8cdbfd8bc20d6b489e50126b3c3c
Pranshu Gupta <pranshumg@gmail.com>
2026-03-20 22:09:06 +0530

    updated code and added comments

--- binary_search/search_in_a_2d_matrix_1.cpp
+++ binary_search/search_in_a_2d_matrix_1.cpp
@@ -2,11 +2,10 @@
 
 using namespace std;
 
-/* Search in a 2D matrix */
+/* Search in a 2D matrix 1 */
 
-// Linear search
 // TC - O(n * m), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
+bool search(vector<vector<int>>& v, int n, int m, int target) {
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
       if (v[i][j] == target) {
@@ -17,46 +16,39 @@ bool search(vector<vector<int>> &v, int n, int m, int target) {
   return false;
 }
 
-// Binary search
-// Better
-bool binary_search(vector<int> &v, int n, int target) {
-  int low = 0, high = n - 1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    if (v[mid] == target) {
-      return true;
-    } else if (v[mid] > target) {
-      high = mid - 1;
-    } else {
-      low = mid + 1;
-    }
-  }
-  return false;
-}
-
-// TC - O(n * log(m)), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
+// TC - O(n + log m), SC - O(1)
+bool search(vector<vector<int>>& v, int n, int m, int target) {
   for (int i = 0; i < n; ++i) {
-    if (binary_search(v[i], m, target)) {
-      return true;
+    if (v[i][0] <= target && target <= v[i][m - 1]) {
+      int low = 0, high = m - 1;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (v[i][mid] == target) {
+          return true;
+        } else if (v[i][mid] < target) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+      break;
     }
   }
   return false;
 }
 
-// Optimal
 // TC - O(log(n * m)), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
+bool search(vector<vector<int>>& v, int n, int m, int target) {
   int low = 0, high = n * m - 1;
   while (low <= high) {
-    int mid = (low + high) / 2;
+    int mid = low + (high - low) / 2;
     int row = mid / m, col = mid % m;
     if (v[row][col] == target) {
-      return true; 
-    } else if (v[row][col] > target) {
-      high = mid - 1;
-    } else {
+      return true;
+    } else if (v[row][col] < target) {
       low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
   return false;
