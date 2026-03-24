638ee0a4e48929db56564c5397d09db8cfca602c
Pranshu Gupta <pranshumg@gmail.com>
2026-03-24 10:55:24 +0530

    updated code

--- binary_search/search_in_a_2d_matrix_2.cpp
+++ binary_search/search_in_a_2d_matrix_2.cpp
@@ -2,27 +2,12 @@
 
 using namespace std;
 
-/* Search in a 2D matrix - 2 */
+/* Search in a 2D matrix 2 */
 
-// Linear search
-// TC - O(n * m), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < m; ++j) {
-      if (v[i][j] == target) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-// Binary search
-// Better
-bool binary_search(vector<int> &v, int n, int target) {
+bool Binary_search(vector<int>& v, int n, int target) {
   int low = 0, high = n - 1;
   while (low <= high) {
-    int mid = (low + high) / 2;
+    int mid = low + (high - low) / 2;
     if (v[mid] == target) {
       return true;
     } else if (v[mid] > target) {
@@ -34,27 +19,26 @@ bool binary_search(vector<int> &v, int n, int target) {
   return false;
 }
 
-// TC - O(n * log(m)), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
+// TC - O(n log m), SC - O(1)
+bool search(vector<vector<int>>& v, int n, int m, int target) {
   for (int i = 0; i < n; ++i) {
-    if (binary_search(v[i], m, target)) {
+    if (Binary_search(v[i], m, target)) {
       return true;
     }
   }
   return false;
 }
 
-// Optimal
 // TC - O(n + m), SC - O(1)
-bool search(vector<vector<int>> &v, int n, int m, int target) {
+bool search(vector<vector<int>>& v, int n, int m, int target) {
   int row = 0, col = m - 1;
   while (row < n && col >= 0) {
     if (v[row][col] == target) {
       return true;
-    } else if (v[row][col] > target) {
-      col--;
+    } else if (v[row][col] < target) {
+      ++row;
     } else {
-      row++;
+      --col;
     }
   }
   return false;
