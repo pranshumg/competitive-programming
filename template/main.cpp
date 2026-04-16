757a800cd766b304cc264aec822c80c33c4a9961
Pranshu Gupta <pranshumg@gmail.com>
2026-04-17 05:20:04 +0530

    initial commit

---
+++ template/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef PRANSHU_DEBUG
+#include "debug.h"
+#else
+#define dbg(...) 24
+#endif
+
+void solve() {
+    $0
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
