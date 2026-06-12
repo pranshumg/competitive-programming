c1d49884345b62244ae23d4676cabbc669d59f5d
Pranshu Gupta <pranshumg@gmail.com>
2026-06-13 05:21:03 +0530

    added wrapper function

--- recursion/combination_sum_1.cpp
+++ recursion/combination_sum_1.cpp
@@ -5,15 +5,22 @@ using namespace std;
 /* Combination sum 1 */
 // https://leetcode.com/problems/combination-sum/
 
-// TC - O(n^(t/m)), SC - O(t/m) where n = no. of candidates, t = target, m = minimum value in candidates
-void find_combination(int idx, int n, vector<int>& v, int target, vector<int>& tmp, vector<vector<int>>& res) {
+void find_combination(int idx, int n, int target, vector<int>& v, vector<int>& tmp, vector<vector<int>>& res) {
     if (target == 0) {
         res.push_back(tmp);
         return;
     }
     if (idx >= n || target < 0) return;
     tmp.push_back(v[idx]);
-    find_combination(idx, n, v, target - v[idx], tmp, res);
+    find_combination(idx, n, target - v[idx], v, tmp, res);
     tmp.pop_back();
-    find_combination(idx + 1, n, v, target, tmp, res);
+    find_combination(idx + 1, n, target, v, tmp, res);
+}
+
+// TC - O(n^(t/m)), SC - O(t/m) where n = no. of candidates, t = target, m = minimum value in candidates
+vector<vector<int>> combination_sum(vector<int>& v, int target) {
+    vector<int> tmp;
+    vector<vector<int>> res; 
+    find_combination(0, int(v.size()), target, v, tmp, res);
+    return res;
 }
