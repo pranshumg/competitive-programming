5f6b6043cbcead149f035218a92691b6ff067b7b
Pranshu Gupta <pranshumg@gmail.com>
2026-06-13 21:02:22 +0530

    added hashet sol

--- recursion/unique_subsets.cpp
+++ recursion/unique_subsets.cpp
@@ -5,6 +5,28 @@ using namespace std;
 /* Unique subsets */
 // https://leetcode.com/problems/subsets-ii/
 
+void find_subset(int idx, int n, vector<int>& v, vector<int>& tmp, set<vector<int>>& st) {
+    if (idx >= n) {
+        st.insert(tmp);
+        return;
+    }
+    tmp.push_back(v[idx]);
+    find_subset(idx + 1, n, v, tmp, st);
+    tmp.pop_back();
+    find_subset(idx + 1, n, v, tmp, st);
+}
+
+// Hashset
+// TC - O(2^n * n * n), SC - O(2^n * n) 
+vector<vector<int>> combination_sum_2(vector<int>& v, int n) {
+    sort(v.begin(), v.end());
+    vector<int> tmp;
+    set<vector<int>> st;
+    find_subset(0, n, v, tmp, st);
+    vector<vector<int>> res(st.begin(), st.end()); 
+    return res;
+}
+
 void find_subset(int idx, int n, vector<int>& v, vector<int>& tmp, vector<vector<int>>& res) {
     res.push_back(tmp);
     for (int i = idx; i < n; i++) {
@@ -17,7 +39,7 @@ void find_subset(int idx, int n, vector<int>& v, vector<int>& tmp, vector<vector
     }
 }
 
-// TC - O(2^n), SC - O(n)
+// TC - O(2^n * n), SC - O(2^n * n)
 vector<vector<int>> subsets(vector<int>& v, int n) {
     sort(v.begin(), v.end());
     vector<int> tmp;
